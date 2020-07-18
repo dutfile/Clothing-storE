ef3c2479b530323fdb5e3eb0db243e3b0e06da58
dutfile <113059452+dutfile@users.noreply.github.com>
2020-07-18 08:13:56 +0000

    prof

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageDebugInfoFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageDebugInfoFeature.java
@@ -36,4 +36,95 @@ import com.oracle.graal.pointsto.util.GraalAccess;
 import com.oracle.graal.pointsto.util.Timer;
 import com.oracle.graal.pointsto.util.TimerCollection;
 import com.oracle.objectfile.BasicProgbitsSectionImpl;
-import com.oracle.objectfile.debuginfo.DebugInfoP
+import com.oracle.objectfile.debuginfo.DebugInfoProvider;
+import com.oracle.objectfile.io.AssemblyBuffer;
+import com.oracle.svm.core.SubstrateOptions;
+import com.oracle.svm.core.UniqueShortNameProvider;
+import com.oracle.svm.core.UniqueShortNameProviderDefaultImpl;
+import com.oracle.svm.core.feature.AutomaticallyRegisteredFeature;
+import com.oracle.svm.core.feature.InternalFeature;
+import com.oracle.svm.core.option.HostedOptionValues;
+import com.oracle.svm.hosted.FeatureImpl;
+import com.oracle.svm.hosted.ProgressReporter;
+import com.oracle.svm.hosted.image.sources.SourceManager;
+import com.oracle.svm.hosted.util.DiagnosticUtils;
+
+@AutomaticallyRegisteredFeature
+@SuppressWarnings("unused")
+class NativeImageDebugInfoFeature implements InternalFeature {
+
+    @Override
+    public boolean isInConfiguration(IsInConfigurationAccess access) {
+        return SubstrateOptions.GenerateDebugInfo.getValue() > 0 && !SubstrateOptions.UseOldDebugInfo.getValue();
+    }
+
+    @Override
+    public void afterRegistration(AfterRegistrationAccess access) {
+        /*
+         * Ensure that the Linux debug unique short name provider is registered when generating
+         * debug info for Linux.
+         */
+        if (!UniqueShortNameProviderDefaultImpl.UseDefault.useDefaultProvider()) {
+            if (!ImageSingletons.contains(UniqueShortNameProvider.class)) {
+                // configure a BFD mangler to provide unique short names for method and field
+                // symbols
+                FeatureImpl.AfterRegistrationAccessImpl accessImpl = (FeatureImpl.AfterRegistrationAccessImpl) access;
+                // the Graal system loader will not duplicate JDK builtin loader classes
+                ClassLoader systemLoader = ClassLoader.getSystemClassLoader();
+                // the Graal app loader and image loader and their parent loader will not duplicate
+                // classes
+                ClassLoader appLoader = accessImpl.getApplicationClassLoader();
+                ClassLoader imageLoader = accessImpl.getImageClassLoader().getClassLoader();
+                ClassLoader imageLoaderParent = imageLoader.getParent();
+                // the app and image loader should both have the same parent
+                assert imageLoaderParent == appLoader.getParent();
+                // ensure the mangle ignores prefix generation for Graal loaders
+                List<ClassLoader> ignored = List.of(systemLoader, imageLoaderParent, appLoader, imageLoader);
+                ImageSingletons.add(UniqueShortNameProvider.class, new NativeImageBFDNameProvider(ignored));
+            }
+        }
+    }
+
+    @Override
+    @SuppressWarnings("try")
+    public void beforeImageWrite(BeforeImageWriteAccess access) {
+        Timer timer = TimerCollection.singleton().get(TimerCollection.Registry.DEBUG_INFO);
+        try (Timer.StopTimer t = timer.start()) {
+            ImageSingletons.add(SourceManager.class, new SourceManager());
+            var accessImpl = (FeatureImpl.BeforeImageWriteAccessImpl) access;
+            var image = accessImpl.getImage();
+            var debugContext = new DebugContext.Builder(HostedOptionValues.singleton(), new GraalDebugHandlersFactory(GraalAccess.getOriginalSnippetReflection())).build();
+            DebugInfoProvider provider = new NativeImageDebugInfoProvider(debugContext, image.getCodeCache(), image.getHeap(), accessImpl.getHostedMetaAccess());
+            var objectFile = image.getObjectFile();
+            objectFile.installDebugInfo(provider);
+
+            if (Platform.includedIn(Platform.LINUX.class) && SubstrateOptions.UseImagebuildDebugSections.getValue()) {
+                /*-
+                 * Provide imagebuild infos as special debug.svm.imagebuild.* sections
+                 * The contents of these sections can be dumped with:
+                 * readelf -p .<sectionName> <debuginfo file>
+                 * e.g. readelf -p .debug.svm.imagebuild.arguments helloworld
+                 */
+                Function<List<String>, BasicProgbitsSectionImpl> makeSectionImpl = customInfo -> {
+                    var content = AssemblyBuffer.createOutputAssembler(objectFile.getByteOrder());
+                    for (String elem : customInfo) {
+                        content.writeString(elem);
+                    }
+                    return new BasicProgbitsSectionImpl(content.getBlob()) {
+                        @Override
+                        public boolean isLoadable() {
+                            return false;
+                        }
+                    };
+                };
+
+                var imageClassLoader = accessImpl.getImageClassLoader();
+                objectFile.newUserDefinedSection(".debug.svm.imagebuild.classpath", makeSectionImpl.apply(DiagnosticUtils.getClassPath(imageClassLoader)));
+                objectFile.newUserDefinedSection(".debug.svm.imagebuild.modulepath", makeSectionImpl.apply(DiagnosticUtils.getModulePath(imageClassLoader)));
+                objectFile.newUserDefinedSection(".debug.svm.imagebuild.arguments", makeSectionImpl.apply(DiagnosticUtils.getBuilderArguments(imageClassLoader)));
+                objectFile.newUserDefinedSection(".debug.svm.imagebuild.java.properties", makeSectionImpl.apply(DiagnosticUtils.getBuilderProperties()));
+            }
+        }
+        ProgressReporter.singleton().setDebugInfoTimer(timer);
+    }
+}
