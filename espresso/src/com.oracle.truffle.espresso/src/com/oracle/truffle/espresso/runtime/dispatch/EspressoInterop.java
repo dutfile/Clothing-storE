2d33e8f45e18800ccea48549e759ee6c90949932
dutfile <113059452+dutfile@users.noreply.github.com>
2020-08-22 20:37:01 +0000

    expanded repos matrix + indentation

---
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/dispatch/EspressoInterop.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2020, 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.oracle.truffle.espresso.runtime.dispatch;
+
+import static com.oracle.truffle.espresso.impl.Klass.STATIC_TO_CLASS;
+import static com.oracle.truffle.espresso.runtime.InteropUtils.isAtMostByte;
+import static com.oracle.truffle.espresso.runtime.InteropUtils.isAtMostFloat;
+import static com.oracle.truffle.espresso.runtime.InteropUtils.isAtMostInt;
+import static com.oracle.truffle.espresso.runtime.InteropUtils.isAtMostLong;
+import static com.oracle.truffle.espresso.runtime.InteropUtils.isAtMostShort;
+import static com.oracle.truffle.espresso.runtime.InteropUtils.isNegativeZero;
+import static com.oracle.truffle.espresso.runtime.StaticObject.CLASS_TO_STATIC;
+import static com.oracle.truffle.espresso.runtime.StaticObject.notNull;
+import static com.oracle.truffle.espresso.vm.InterpreterToVM.instanceOf;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.Cached.Exclusive;
+import com.oracle.truffle.api.dsl.Cached.Shared;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.exception.AbstractTruffleException;
+import com.oracle.truffle.api.interop.ArityException;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.InvalidArrayIndexException;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.interop.UnsupportedTypeException;
+import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.api.profiles.BranchProfile;
+import com.oracle.truffle.espresso.EspressoLanguage;
+import com.oracle.truffle.espresso.impl.ArrayKlass;
+import com.oracle.truffle.espresso.impl.EmptyKeysArray;
+import com.oracle.truffle.espresso.impl.Field;
+import com.oracle.truffle.espresso.impl.KeysArray;
+import com.oracle.truffle.espresso.impl.Klass;
+import com.oracle.truffle.espresso.impl.Method;
+import com.oracle.truffle.espresso.impl.ObjectKlass;
+import com.oracle.truffle.espresso.meta.EspressoError;
+import com.oracle.truffle.espresso.meta.Meta;
+import com.oracle.truffle.espresso.nodes.interop.CandidateMethodWithArgs;
+import com.oracle.truffle.espresso.nodes.interop.InvokeEspressoNode;
+import com.oracle.truffle.espresso.nodes.interop.LookupInstanceFieldNode;
+import com.oracle.truffle.espresso.nodes.interop.LookupVirtualMethodNode;
+import com.oracle.truffle.espresso.nodes.interop.MethodArgsUtils;
+import com.oracle.truffle.espresso.nodes.interop.OverLoadedMethodSelectorNode;
+import com.oracle.truffle.espresso.nodes.interop.ToEspressoNode;
+import com.oracle.truffle.espresso.runtime.EspressoContext;
+import com.oracle.truffle.espresso.runtime.EspressoException;
+import com.oracle.truffle.espresso.runtime.EspressoFunction;
+import com.oracle.truffle.espresso.runtime.InteropUtils;
+import com.oracle.truffle.espresso.runtime.StaticObject;
+
+/**
+ * BaseInterop (isNull, is/asString, meta-instance, identity, exceptions, toDisplayString) Support
+ * Espresso and foreign objects and null.
+ */
+@ExportLibrary(value = InteropLibrary.class, receiverType = StaticObject.class)
+@SuppressWarnings("truffle-abstract-export") // TODO GR-44080 Adopt BigInteger Interop
+public class EspressoInterop extends BaseInterop {
+    // region ### is/as checks/conversions
+
+    static final Object[] EMPTY_ARGS = new Object[]{};
+
+    public static Meta getMeta() {
+        CompilerAsserts.neverPartOfCompilation();
+        return EspressoContext.get(null).getMeta();
+    }
+
+    @ExportMessage
+    static boolean isBoolean(StaticObject receiver) {
+        if (receiver.isForeignObject()) {
+            return false;
+        }
+        if (isNull(receiver)) {
+            return false;
+        }
+        return receiver.getKlass() == receiver.getKlass().getMeta().java_lang_Boolean;
+    }
+
+    @ExportMessage
+    static boolean asBoolean(StaticObject receiver) throws UnsupportedMessageException {
+        receiver.checkNotForeign();
+        if (!isBoolean(receiver)) {
+            throw UnsupportedMessageException.create();
+        }
+        return (boolean) receiver.getKlass().getMeta().java_lang_Boolean_value.get(receiver);
+    }
+
+    @ExportMessage
+    static boolean isNumber(StaticObject receiver) {
+        if (receiver.isForeignObject()) {
+            return false;
+        }
+        if (isNull(receiver)) {
+            return false;
+        }
+        Meta meta = receiver.getKlass().getMeta();
+        
