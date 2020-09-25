a934133d7840b19a0eb0669915485e04b5898ad7
dutfile <113059452+dutfile@users.noreply.github.com>
2020-09-25 21:49:58 +0000

    cgroup/kmemleak: add kmemleak_free() for cgroup deallocations.

--- compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/Adler32SubstitutionsTest.java
+++ compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/Adler32SubstitutionsTest.java
@@ -19,4 +19,84 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need a
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.compiler.hotspot.test;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.zip.Adler32;
+
+import org.graalvm.compiler.core.test.GraalCompilerTest;
+import org.junit.Test;
+
+/**
+ * Tests compiled call to {@link java.util.zip.Adler32#update(int)}.
+ */
+@SuppressWarnings("javadoc")
+public class Adler32SubstitutionsTest extends GraalCompilerTest {
+
+    public static long update(byte[] input) {
+        Adler32 adler = new Adler32();
+        for (byte b : input) {
+            adler.update(b);
+        }
+        return adler.getValue();
+    }
+
+    @Test
+    public void test1() {
+        test("update", "some string".getBytes());
+    }
+
+    public static long updateBytes(byte[] input, int offset, int length) {
+        Adler32 adler = new Adler32();
+        adler.update(input, offset, length);
+        return adler.getValue();
+    }
+
+    @Test
+    public void test2() {
+        byte[] buf = "some string".getBytes();
+        int off = 0;
+        int len = buf.length;
+        test("updateBytes", buf, off, len);
+    }
+
+    @Test
+    public void test3() throws Throwable {
+        String classfileName = Adler32SubstitutionsTest.class.getSimpleName().replace('.', '/') + ".class";
+        InputStream s = Adler32SubstitutionsTest.class.getResourceAsStream(classfileName);
+        byte[] buf = new byte[s.available()];
+        new DataInputStream(s).readFully(buf);
+        test("updateBytes", buf, 0, buf.length);
+        for (int offset = 1; offset < buf.length; offset++) {
+            test("updateBytes", buf, offset, buf.length - offset);
+        }
+    }
+
+    public static long updateByteBuffer(ByteBuffer buffer) {
+        Adler32 adler = new Adler32();
+        buffer.rewind();
+        adler.update(buffer);
+        return adler.getValue();
+    }
+
+    @Test
+    public void test4() throws Throwable {
+        String classfileName = Adler32SubstitutionsTest.class.getSimpleName().replace('.', '/') + ".class";
+        InputStream s = Adler32SubstitutionsTest.class.getResourceAsStream(classfileName);
+        byte[] buf = new byte[s.available()];
+        new DataInputStream(s).readFully(buf);
+
+        ByteBuffer directBuf = ByteBuffer.allocateDirect(buf.length);
+        directBuf.put(buf);
+        ByteBuffer heapBuf = ByteBuffer.wrap(buf);
+
+        test("updateByteBuffer", directBuf);
+        test("updateByteBuffer", heapBuf);
+    }
+
+}
---
+++ substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/flow/CloneTypeFlow.java
@@ -0,0 +1,119 @@
+
+/*
+ * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.pointsto.flow;
+
+import com.oracle.graal.pointsto.PointsToAnalysis;
+import com.oracle.graal.pointsto.flow.context.AnalysisContext;
+import com.oracle.graal.pointsto.meta.AnalysisType;
+import com.oracle.graal.pointsto.typestate.TypeState;
+
+import jdk.vm.ci.code.BytecodePosition;
+
+/**
+ * Implements a clone operation. This flow observes the state changes of the input flow, clones its
+ * objects, then it updates its state. When the state is updated it also copies the corresponding
+ * elements, i.e., array elements if the type is array or field values if the type is non-array,
+ * into the clones.
+ */
+public class CloneTypeFlow extends TypeFlow<BytecodePosition> {
+
+    private TypeFlow<?> input;
+    /** The allocation context for the generated clone object. Null if this is not a clone. */
+    protected final AnalysisContext allocationContext;
+
+    public CloneTypeFlow(BytecodePosition cloneLocation, AnalysisType inputType, TypeFlow<?> input) {
+        super(cloneLocation, inputType);
+        this.allocationContext = null;
+        this.input = input;
+    }
+
+    public CloneTypeFlow(PointsToAnalysis bb, CloneTypeFlow original, MethodFlowsGraph methodFlows, AnalysisContext allocationContext) {
+        super(original, methodFlows);
+        this.allocationContext = allocationContext;
+        this.input = methodFlows.lookupCloneOf(bb, original.input);
+    }
+
+    @Override
+    public TypeFlow<BytecodePosition> copy(PointsToAnalysis bb, MethodFlowsGraph methodFlows) {
+        AnalysisContext allocContext = bb.analysisPolicy().allocationContext(bb, methodFlows);
+        return new CloneTypeFlow(bb, this, methodFlows, allocContext);
+    }
+
+    @Override
+    public void onObservedUpdate(PointsToAnalysis bb) {
+        /* The input state has changed, clone its objects. */
+        TypeState inputState = input.getState();
+
+        /*
+         * The clone type flow creates a clone of it's input state. It intercepts the input state
+         * and it creates a new state with the same types. The object abstractions can be the same
+         * or different depending on the analysis policy. If new heap objects are created they
+         * encapsulate the location of the cloning. From the point of view of the analysis a clone
+         * flow is a source.
+         */
+        TypeState resultState = bb.analysisPolicy().cloneState(bb, state, inputState, source, allocationContext);
+
+        /* Update the clone flow state. */
+        addState(bb, resultState);
+    }
+
+    @Override
+    public void update(PointsToAnalysis bb) {
+        /* Link the elements of the cloned objects to the elements of the source objects. */
+        bb.analysisPolicy().linkClonedObjects(bb, input, this, source);
+
+        /* Element flows of array clones (if any) have been updated, update the uses. */
+        super.update(bb);
+    }
+
+    @Override
+    public void onObservedSaturated(PointsToAnalysis bb, TypeFlow<?> observed) {
+        if (!isSaturated()) {
+            /*
+             * When the input flow saturates start observing the flow of the declared type, unless
+             * the clone is already saturated.
+             */
+            replaceObservedWith(bb, declaredType);
+        }
+    }
+
+    @Override
+    protected void onSaturated() {
+        /* Deregister the clone as an observer of the input. */
+        input.removeObserver(this);
+    }
+
+    @Override
+    public void setObserved(TypeFlow<?> newInputFlow) {
+        this.input = newInputFlow;
+    }
+
+    @Override
+    public String toString() {
+        return "Clone<" + super.toString() + ">";
+    }
+
+}
