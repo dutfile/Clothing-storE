3153e734706d310739f38ec097d163d06b54339d
dutfile <113059452+dutfile@users.noreply.github.com>
2020-10-07 17:11:23 +0000

    Delete .travis.yml

--- substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/debugentry/range/Range.java
+++ substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/debugentry/range/Range.java
@@ -11,4 +11,162 @@
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPO
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.oracle.objectfile.debugentry.range;
+
+import com.oracle.objectfile.debugentry.FileEntry;
+import com.oracle.objectfile.debugentry.MethodEntry;
+import com.oracle.objectfile.debugentry.TypeEntry;
+import com.oracle.objectfile.debuginfo.DebugInfoProvider.DebugLocalInfo;
+import com.oracle.objectfile.debuginfo.DebugInfoProvider.DebugLocalValueInfo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Details of a specific address range in a compiled method either a primary range identifying a
+ * whole compiled method or a sub-range identifying a sub-sequence of the compiled instructions that
+ * may derive from top level or inlined code. Each sub-range is linked with its caller, (which may
+ * be the primary range) and its callees, forming a call tree. Subranges are either leaf nodes with
+ * no children or call nodes which have children.
+ *
+ * <ul>
+ * <li>A leaf node at the top level (depth 0) records the start and extent of a sequence of compiled
+ * code derived from the top level method. The leaf node reports itself as belonging to the top
+ * level method.
+ * <li>A leaf node at depth N records the start and extent of a sequence of compiled code derived
+ * from a leaf inlined method at a call depth of N. The leaf node reports itself as belonging to the
+ * leaf method.
+ * <li>A call node at level 0 records the start and extent of a sequence of compiled code that
+ * includes all compiled code derived from a top level call that has been inlined. All child nodes
+ * of the call node (direct or indirect) should model ranges that lie within the parent range. The
+ * call node reports itself as belonging to the top level method and its file and line information
+ * identify the location of the call.
+ * <li>A call node at level N records the start and extent of a sequence of compiled code that
+ * includes all compiled code derived from an inline call at depth N. All child nodes of the call
+ * node (direct or indirect) should model ranges that lie within the parent range. The call node
+ * reports itself as belonging to the caller method at depth N and its file and line information
+ * identify the location of the call.
+ * <ul>
+ *
+ * Ranges also record the location of local and parameter values that are valid for the range's
+ * extent. Each value maps to a corresponding parameter or local variable attached to the range's
+ * method. So, a leaf or call node at level 0 records local and parameter values for separate
+ * sub-extents of the top level method while a leaf or call node at level N+1 records local and
+ * parameter values for separate sub-extents of an inline called method whose full extent is
+ * represented by the parent call range at level N.
+ */
+public abstract class Range {
+    private static final String CLASS_DELIMITER = ".";
+    protected final MethodEntry methodEntry;
+    protected final int lo;
+    protected int hi;
+    protected final int line;
+    protected final int depth;
+
+    /**
+     * Create a primary range representing the root of the subrange tree for a top level compiled
+     * method.
+     * 
+     * @param methodEntry the top level compiled method for this primary range.
+     * @param lo the lowest address included in the range.
+     * @param hi the first address above the highest address in the range.
+     * @param line the line number associated with the range
+     * @return a new primary range to serve as the root of the subrange tree.
+     */
+    public static PrimaryRange createPrimary(MethodEntry methodEntry, int lo, int hi, int line) {
+        return new PrimaryRange(methodEntry, lo, hi, line);
+    }
+
+    /**
+     * Create a subrange representing a segment of the address range for code of a top level or
+     * inlined compiled method. The result will either be a call or a leaf range.
+     * 
+     * @param methodEntry the method from which code in the subrange is derived.
+     * @param lo the lowest address included in the range.
+     * @param hi the first address above the highest address in the range.
+     * @param line the line number associated with the range
+     * @param primary the primary range to which this subrange belongs
+     * @param caller the range for which this is a subrange, either an inlined call range or the
+     *            primary range.
+     * @param isLeaf true if this is a leaf range with no subranges
+     * @return a new subrange to be linked into the range tree below the primary range.
+     */
+    public static SubRange createSubrange(MethodEntry methodEntry, int lo, int hi, int line, PrimaryRange primary, Range caller, boolean isLeaf) {
+        assert primary != null;
+        assert primary.isPrimary();
+        if (isLeaf) {
+            return new LeafRange(methodEntry, lo, hi, line, primary, caller);
+        } else {
+            return new CallRange(methodEntry, lo, hi, line, primary, caller);
+        }
+    }
+
+    protected Range(MethodEntry methodEntry, int lo, int hi, int line, int depth) {
+        assert methodEntry != null;
+        this.methodEntry = methodEntry;
+        this.lo = lo;
+        this.hi = hi;
+        this.line = line;
+        this.depth = depth;
+    }
+
+    protected abstract void addCallee(SubRange callee);
+
+    public boolean contains(Range other) {
+        return (lo <= other.lo && hi >= other.hi);
+    }
+
+    public abstract boolean isPrimary();
+
+    public String getClassName() {
+        return methodEntry.ownerType().getTypeName();
+    }
+
+    public String getMethodName() {
+        return methodEntry.methodName();
+    }
+
+    public String getSymbolName() {
+        return methodEntry.getSymbolName();
+    }
+
+    public int getHi() {
+        return hi;
+    }
+
+    public int getLo() {
+        return lo;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public String getFullMethodName() {
+        return constructClassAndMethodName();
+    }
+
+    public String getFullMethodNameWithParams() {
+        return constructClassAndMethodNameWithParams();
+    }
+
+    public boolean isDeoptTarget() {
+        return methodEntry.isDeopt();
+    }
+
+    private String getExtendedMethodName(boolean includeClass, boolean includeParams, boolean includeReturnType) {
+        StringBuilder builder
