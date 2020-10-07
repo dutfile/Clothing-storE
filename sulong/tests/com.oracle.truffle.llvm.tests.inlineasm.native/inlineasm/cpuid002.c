1355a371cca43e2f134c711a8551bc5ea450e0cc
dutfile <113059452+dutfile@users.noreply.github.com>
2020-10-07 14:06:38 +0000

    update Thallium

--- sulong/tests/com.oracle.truffle.llvm.tests.inlineasm.native/inlineasm/cpuid002.c
+++ sulong/tests/com.oracle.truffle.llvm.tests.inlineasm.native/inlineasm/cpuid002.c
@@ -13,4 +13,48 @@
  * conditions and the following disclaimer in the documentation and/or other materials provided
  * with the distribution.
  *
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <cpuid.h>
+#include <string.h>
+
+typedef union {
+    struct {
+        unsigned int b, d, c;
+    };
+    char str[13];
+} VENDOR;
+
+int main(void) {
+    VENDOR vendor;
+    unsigned int a;
+
+    __cpuid(0, a, vendor.b, vendor.c, vendor.d);
+    vendor.str[12] = 0;
+    if (!strcmp(vendor.str, "GenuineIntel")) {
+        return 1;
+    }
+    if (!strcmp(vendor.str, "AuthenticAMD")) {
+        return 1;
+    }
+    if (!strcmp(vendor.str, "KVMKVMKVM")) {
+        return 1;
+    }
+    if (!strcmp(vendor.str, "SulongLLVM64")) {
+        return 1;
+    }
+
+    return 0;
+}
