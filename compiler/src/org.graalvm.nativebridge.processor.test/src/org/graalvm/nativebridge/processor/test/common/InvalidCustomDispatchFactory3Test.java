a7d80b9d88dd38693c1d4fafb0c97c7616f845e5
dutfile <113059452+dutfile@users.noreply.github.com>
2020-12-29 06:59:35 +0000

    updated CSS

--- compiler/src/org.graalvm.nativebridge.processor.test/src/org/graalvm/nativebridge/processor/test/common/InvalidCustomDispatchFactory3Test.java
+++ compiler/src/org.graalvm.nativebridge.processor.test/src/org/graalvm/nativebridge/processor/test/common/InvalidCustomDispatchFactory3Test.java
@@ -15,4 +15,41 @@
  * accompanied this code).
  *
  * You should have received a copy of the GNU General Public License version
- * 2 alo
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.nativebridge.processor.test.common;
+
+import org.graalvm.nativebridge.CustomDispatchAccessor;
+import org.graalvm.nativebridge.CustomDispatchFactory;
+import org.graalvm.nativebridge.CustomReceiverAccessor;
+import org.graalvm.nativebridge.GenerateNativeToHotSpotBridge;
+import org.graalvm.nativebridge.processor.test.CustomReceiverService;
+import org.graalvm.nativebridge.processor.test.ExpectError;
+import org.graalvm.nativebridge.processor.test.ServiceAPI;
+import org.graalvm.nativebridge.processor.test.TestJNIConfig;
+
+@GenerateNativeToHotSpotBridge(jniConfig = TestJNIConfig.class)
+abstract class InvalidCustomDispatchFactory3Test extends CustomReceiverService {
+
+    @CustomDispatchAccessor
+    static CustomReceiverService getDispatch(ServiceAPI receiver) {
+        return receiver.dispatch;
+    }
+
+    @CustomReceiverAccessor
+    static Object getReceiver(ServiceAPI receiver) {
+        return receiver.receiver;
+    }
+
+    @CustomDispatchFactory
+    @ExpectError("A method annotated by `CustomDispatchFactory` must be a non-private static method with a single object parameter and `ServiceAPI` return type.%n" +
+                    "To fix this change the signature to `static ServiceAPI create(Object receiver)`.")
+    private static ServiceAPI create(Object receiver) {
+        return new ServiceAPI(null, receiver);
+    }
+}
