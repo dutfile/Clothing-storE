be7b5a5dd17b303ba0f81ab784ee85ecf2f06c86
dutfile <113059452+dutfile@users.noreply.github.com>
2021-01-07 11:56:44 +0000

    tracing: Fix unmapping loop in tracing_mark_write

--- sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/model/enums/ReadModifyWriteOperator.java
+++ sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/model/enums/ReadModifyWriteOperator.java
@@ -24,4 +24,46 @@
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
  * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF T
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.oracle.truffle.llvm.parser.model.enums;
+
+public enum ReadModifyWriteOperator {
+
+    XCHG("xchg"),
+    ADD("add"),
+    SUB("sub"),
+    AND("and"),
+    NAND("nand"),
+    OR("or"),
+    XOR("xor"),
+    MAX("max"),
+    MIN("min"),
+    UMAX("umax"),
+    UMIN("umin"),
+    FADD("fadd"),
+    FSUB("fsub");
+
+    private static final ReadModifyWriteOperator[] VALUES = values();
+
+    public static ReadModifyWriteOperator decode(int opcode) {
+        if (opcode >= 0 && opcode < VALUES.length) {
+            return VALUES[opcode];
+        }
+        return null;
+    }
+
+    private final String irString;
+
+    ReadModifyWriteOperator(String irString) {
+        this.irString = irString;
+    }
+
+    /**
+     * Useful to get the llvm ir equivalent string of the enum.
+     */
+    public String getIrString() {
+        return irString;
+    }
+}
