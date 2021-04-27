b7940f4a74367922ddd142349e9fdc2a710cc8ca
dutfile <113059452+dutfile@users.noreply.github.com>
2021-04-27 22:43:09 +0000

    adding bootstrap

--- compiler/src/org.graalvm.jniutils/src/org/graalvm/jniutils/JNI.java
+++ compiler/src/org.graalvm.jniutils/src/org/graalvm/jniutils/JNI.java
@@ -82,4 +82,239 @@ public final class JNI {
         int MODE_RELEASE = 2;
     }
 
-    public interface JBooleanArra
+    public interface JBooleanArray extends JArray {
+    }
+
+    public interface JByteArray extends JArray {
+    }
+
+    public interface JCharArray extends JArray {
+    }
+
+    public interface JShortArray extends JArray {
+    }
+
+    public interface JIntArray extends JArray {
+    }
+
+    public interface JLongArray extends JArray {
+    }
+
+    public interface JFloatArray extends JArray {
+    }
+
+    public interface JDoubleArray extends JArray {
+    }
+
+    public interface JObjectArray extends JArray {
+    }
+
+    public interface JClass extends JObject {
+    }
+
+    public interface JString extends JObject {
+    }
+
+    public interface JThrowable extends JObject {
+    }
+
+    public interface JWeak extends JObject {
+    }
+
+    /**
+     * Access to the {@code jvalue} JNI union.
+     *
+     * <pre>
+     * typedef union jvalue {
+     *    jboolean z;
+     *    jbyte    b;
+     *    jchar    c;
+     *    jshort   s;
+     *    jint     i;
+     *    jlong    j;
+     *    jfloat   f;
+     *    jdouble  d;
+     *    jobject  l;
+     * } jvalue;
+     * </pre>
+     */
+    @CContext(JNIHeaderDirectives.class)
+    @CStruct("jvalue")
+    public interface JValue extends PointerBase {
+        // @formatter:off
+        @CField("z")    boolean getBoolean();
+        @CField("b")    byte    getByte();
+        @CField("c")    char    getChar();
+        @CField("s")    short   getShort();
+        @CField("i")    int     getInt();
+        @CField("j")    long    getLong();
+        @CField("f")    float   getFloat();
+        @CField("d")    double  getDouble();
+        @CField("l")    JObject getJObject();
+
+        @CField("z")    void setBoolean(boolean b);
+        @CField("b")    void setByte(byte b);
+        @CField("c")    void setChar(char ch);
+        @CField("s")    void setShort(short s);
+        @CField("i")    void setInt(int i);
+        @CField("j")    void setLong(long l);
+        @CField("f")    void setFloat(float f);
+        @CField("d")    void setDouble(double d);
+        @CField("l")    void setJObject(JObject obj);
+        // @formatter:on
+
+        /**
+         * Gets JValue in an array of JValues pointed to by this object.
+         */
+        JValue addressOf(int index);
+    }
+
+    @CContext(JNIHeaderDirectives.class)
+    @CStruct(value = "JNIEnv_", addStructKeyword = true)
+    public interface JNIEnv extends PointerBase {
+        @CField("functions")
+        JNINativeInterface getFunctions();
+    }
+
+    @CPointerTo(JNIEnv.class)
+    public interface JNIEnvPointer extends PointerBase {
+        JNIEnv readJNIEnv();
+
+        void writeJNIEnv(JNIEnv env);
+    }
+
+    @CContext(JNIHeaderDirectives.class)
+    @CStruct(value = "JNINativeInterface_", addStructKeyword = true)
+    public interface JNINativeInterface extends PointerBase {
+
+        @CField("NewString")
+        NewString getNewString();
+
+        @CField("GetStringLength")
+        GetStringLength getGetStringLength();
+
+        @CField("GetStringChars")
+        GetStringChars getGetStringChars();
+
+        @CField("ReleaseStringChars")
+        ReleaseStringChars getReleaseStringChars();
+
+        @CField("NewStringUTF")
+        NewStringUTF8 getNewStringUTF();
+
+        @CField("GetStringUTFLength")
+        GetStringUTFLength getGetStringUTFLength();
+
+        @CField("GetStringUTFChars")
+        GetStringUTFChars getGetStringUTFChars();
+
+        @CField("ReleaseStringUTFChars")
+        ReleaseStringUTFChars getReleaseStringUTFChars();
+
+        @CField("GetArrayLength")
+        GetArrayLength getGetArrayLength();
+
+        @CField("NewObjectArray")
+        NewObjectArray getNewObjectArray();
+
+        @CField("NewBooleanArray")
+        NewBooleanArray getNewBooleanArray();
+
+        @CField("NewByteArray")
+        NewByteArray getNewByteArray();
+
+        @CField("NewCharArray")
+        NewCharArray getNewCharArray();
+
+        @CField("NewShortArray")
+        NewShortArray getNewShortArray();
+
+        @CField("NewIntArray")
+        NewIntArray getNewIntArray();
+
+        @CField("NewLongArray")
+        NewLongArray getNewLongArray();
+
+        @CField("NewFloatArray")
+        NewFloatArray getNewFloatArray();
+
+        @CField("NewDoubleArray")
+        NewDoubleArray getNewDoubleArray();
+
+        @CField("GetObjectArrayElement")
+        GetObjectArrayElement getGetObjectArrayElement();
+
+        @CField("SetObjectArrayElement")
+        SetObjectArrayElement getSetObjectArrayElement();
+
+        @CField("GetBooleanArrayElements")
+        GetBooleanArrayElements getGetBooleanArrayElements();
+
+        @CField("GetByteArrayElements")
+        GetByteArrayElements getGetByteArrayElements();
+
+        @CField("GetCharArrayElements")
+        GetCharArrayElements getGetCharArrayElements();
+
+        @CField("GetShortArrayElements")
+        GetShortArrayElements getGetShortArrayElements();
+
+        @CField("GetIntArrayElements")
+        GetIntArrayElements getGetIntArrayElements();
+
+        @CField("GetLongArrayElements")
+        GetLongArrayElements getGetLongArrayElements();
+
+        @CField("GetFloatArrayElements")
+        GetFloatArrayElements getGetFloatArrayElements();
+
+        @CField("GetDoubleArrayElements")
+        GetDoubleArrayElements getGetDoubleArrayElements();
+
+        @CField("ReleaseBooleanArrayElements")
+        ReleaseBooleanArrayElements getReleaseBooleanArrayElements();
+
+        @CField("ReleaseByteArrayElements")
+        ReleaseByteArrayElements getReleaseByteArrayElements();
+
+        @CField("ReleaseCharArrayElements")
+        ReleaseCharArrayElements getReleaseCharArrayElements();
+
+        @CField("ReleaseShortArrayElements")
+        ReleaseShortArrayElements getReleaseShortArrayElements();
+
+        @CField("ReleaseIntArrayElements")
+        ReleaseIntArrayElements getReleaseIntArrayElements();
+
+        @CField("ReleaseLongArrayElements")
+        ReleaseLongArrayElements getReleaseLongArrayElements();
+
+        @CField("ReleaseFloatArrayElements")
+        ReleaseFloatArrayElements getReleaseFloatArrayElements();
+
+        @CField("ReleaseDoubleArrayElements")
+        ReleaseDoubleArrayElements getReleaseDoubleArrayElements();
+
+        @CField("GetBooleanArrayRegion")
+        GetBooleanArrayRegion getGetBooleanArrayRegion();
+
+        @CField("GetByteArrayRegion")
+        GetByteArrayRegion getGetByteArrayRegion();
+
+        @CField("GetCharArrayRegion")
+        GetCharArrayRegion getGetCharArrayRegion();
+
+        @CField("GetShortArrayRegion")
+        GetShortArrayRegion getGetShortArrayRegion();
+
+        @CField("GetIntArrayRegion")
+        GetIntArrayRegion getGetIntArrayRegion();
+
+        @CField("GetLongArrayRegion")
+        GetLongArrayRegion getGetLongArrayRegion();
+
+        @CField("GetFloatArrayRegion")
+        GetFloatArrayRegion getGetFloatArrayRegion();
+
+        @CField("GetDoubleArrayRegion")
+        
