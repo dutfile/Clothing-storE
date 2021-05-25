e7c7311e5d47254a3730c3ef3c33e58cfdaebd04
dutfile <113059452+dutfile@users.noreply.github.com>
2021-05-25 20:08:18 +0000

    Delete changelog

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/virtual/VirtualObjectNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/virtual/VirtualObjectNode.java
@@ -88,4 +88,80 @@ public abstract class VirtualObjectNode extends ValueNode implements LIRLowerabl
     public abstract int entryCount();
 
     /**
-     * Returns the name of the entry at the given index. Only used for debugg
+     * Returns the name of the entry at the given index. Only used for debugging purposes.
+     */
+    public abstract String entryName(int i);
+
+    /**
+     * If the given index denotes an entry in this virtual object, the index of this entry is
+     * returned. If no such entry can be found, this method returns -1.
+     *
+     * @param constantOffset offset, where the value is placed.
+     * @param expectedEntryKind Specifies which type is expected at this offset (Is important when
+     */
+    public abstract int entryIndexForOffset(MetaAccessProvider metaAccess, long constantOffset, JavaKind expectedEntryKind);
+
+    /**
+     * Returns the {@link JavaKind} of the entry at the given index.
+     */
+    public abstract JavaKind entryKind(MetaAccessExtensionProvider metaAccessExtensionProvider, int index);
+
+    /**
+     * Returns an exact duplicate of this virtual object node, which has not been added to the graph
+     * yet.
+     */
+    public abstract VirtualObjectNode duplicate();
+
+    /**
+     * Specifies whether this virtual object has an object identity. If not, then the result of a
+     * comparison of two virtual objects is determined by comparing their contents.
+     */
+    public boolean hasIdentity() {
+        return hasIdentity;
+    }
+
+    public void setIdentity(boolean identity) {
+        this.hasIdentity = identity;
+    }
+
+    /**
+     * Returns a node that can be used to materialize this virtual object. If this returns an
+     * {@link AllocatedObjectNode} then this node will be attached to a {@link CommitAllocationNode}
+     * , otherwise the node will just be added to the graph.
+     */
+    public abstract ValueNode getMaterializedRepresentation(FixedNode fixed, ValueNode[] entries, LockState locks);
+
+    @Override
+    public void generate(NodeLIRBuilderTool gen) {
+        // nothing to do...
+    }
+
+    /**
+     * Checks that a read in a virtual object is a candidate for byte array virtualization.
+     *
+     * Virtualizing reads in byte arrays can happen iff all of these hold true:
+     * <li>The virtualized object is a virtualized byte array
+     * <li>Both the virtualized entry and the access kind are primitives
+     * <li>The number of bytes actually occupied by the entry is equal to the number of bytes of the
+     * access kind
+     */
+    public boolean canVirtualizeLargeByteArrayUnsafeRead(ValueNode entry, int index, JavaKind accessKind, VirtualizerTool tool) {
+        return (tool.canVirtualizeLargeByteArrayUnsafeAccess() || accessKind == JavaKind.Byte) &&
+                        !entry.isIllegalConstant() && entry.getStackKind() == accessKind.getStackKind() &&
+                        isVirtualByteArrayAccess(tool.getMetaAccessExtensionProvider(), accessKind) &&
+                        accessKind.getByteCount() == ((VirtualArrayNode) this).byteArrayEntryByteCount(index, tool);
+    }
+
+    public boolean isVirtualByteArrayAccess(MetaAccessExtensionProvider metaAccessExtensionProvider, JavaKind accessKind) {
+        return accessKind.isPrimitive() && isVirtualByteArray(metaAccessExtensionProvider);
+    }
+
+    public boolean isVirtualByteArray(MetaAccessExtensionProvider metaAccessExtensionProvider) {
+        return isVirtualArray() && entryCount() > 0 && entryKind(metaAccessExtensionProvider, 0) == JavaKind.Byte;
+    }
+
+    private boolean isVirtualArray() {
+        return this instanceof VirtualArrayNode;
+    }
+
+}
