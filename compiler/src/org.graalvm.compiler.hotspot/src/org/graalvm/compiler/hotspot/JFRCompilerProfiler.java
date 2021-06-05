a7c3f9d2f4a075b999e22bc359a598993f7bbc9b
dutfile <113059452+dutfile@users.noreply.github.com>
2021-06-05 18:00:24 +0000

    Update moment

--- compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/JFRCompilerProfiler.java
+++ compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/JFRCompilerProfiler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,9 +22,34 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+package org.graalvm.compiler.hotspot;
 
-@Platforms(Platform.HOSTED_ONLY.class)
-package com.oracle.svm.hosted.config;
+import org.graalvm.compiler.core.common.CompilerProfiler;
+import org.graalvm.compiler.serviceprovider.ServiceProvider;
 
-import org.graalvm.nativeimage.Platform;
-import org.graalvm.nativeimage.Platforms;
+import jdk.vm.ci.hotspot.JFR;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+
+/**
+ * A HotSpot JFR implementation of {@link CompilerProfiler}.
+ */
+@ServiceProvider(CompilerProfiler.class)
+public final class JFRCompilerProfiler implements CompilerProfiler {
+
+    @Override
+    public long getTicks() {
+        return JFR.Ticks.now();
+    }
+
+    @Override
+    public void notifyCompilerPhaseEvent(int compileId, long startTime, String name, int nestingLevel) {
+        JFR.CompilerPhaseEvent.write(startTime, name, compileId, nestingLevel);
+    }
+
+    @Override
+    public void notifyCompilerInlingEvent(int compileId, ResolvedJavaMethod caller, ResolvedJavaMethod callee,
+                    boolean succeeded, String message, int bci) {
+        JFR.CompilerInliningEvent.write(compileId, caller, callee, succeeded, message, bci);
+    }
+
+}
