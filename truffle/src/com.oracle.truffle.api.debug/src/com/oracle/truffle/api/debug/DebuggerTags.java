903b1fb61e117ecb6f359bf5730e523650e5b6cd
dutfile <113059452+dutfile@users.noreply.github.com>
2021-07-11 13:08:35 +0000

    fix regressions

--- truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/DebuggerTags.java
+++ truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/DebuggerTags.java
@@ -20,4 +20,81 @@
  *
  * without restriction, including without limitation the rights to copy, create
  * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.oracle.truffle.api.debug;
+
+import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.instrumentation.InstrumentableNode;
+import com.oracle.truffle.api.instrumentation.ProvidedTags;
+import com.oracle.truffle.api.instrumentation.Tag;
+import com.oracle.truffle.api.nodes.Node;
+
+/**
+ * Set of debugger-specific tags. Language should {@link ProvidedTags provide} an implementation of
+ * these tags in order to support specific debugging features.
+ *
+ * @since 0.14
+ */
+public final class DebuggerTags {
+
+    private DebuggerTags() {
+        // No instances
+    }
+
+    /**
+     * Marks program locations where debugger should always halt like if on a breakpoint.
+     * <p>
+     * {@link TruffleLanguage}s that support concept similar to JavaScript's <code>debugger</code>
+     * statement (program locations where execution should always halt) should make sure that
+     * appropriate {@link Node}s are tagged with the {@link AlwaysHalt} tag.
+     *
+     * {@link com.oracle.truffle.api.debug.DebuggerTagsSnippets#debuggerNode}
+     *
+     * All created {@link DebuggerSession debugger sessions} will suspend on these locations
+     * unconditionally.
+     *
+     * @since 0.14
+     */
+    public final class AlwaysHalt extends Tag {
+        private AlwaysHalt() {
+            /* No instances */
+        }
+    }
+
+}
+
+class DebuggerTagsSnippets {
+
+    @SuppressWarnings("unused")
+    public static Node debuggerNode() {
+        // @formatter:off
+        abstract
+        // BEGIN: com.oracle.truffle.api.debug.DebuggerTagsSnippets#debuggerNode
+        class DebuggerNode extends Node implements InstrumentableNode {
+
+            public boolean hasTag(Class<? extends Tag> tag) {
+                return tag == DebuggerTags.AlwaysHalt.class;
+            }
+        }
+        // END: com.oracle.truffle.api.debug.DebuggerTagsSnippets#debuggerNode
+        // @formatter:on
+        return null;
+    }
+}
