f1d624ac9c0162329ed4cdfe7234f90ed822c20a
dutfile <113059452+dutfile@users.noreply.github.com>
2021-07-16 20:57:10 +0000

    updated schema

--- vm/src/org.graalvm.component.installer/src/org/graalvm/component/installer/Archive.java
+++ vm/src/org.graalvm.component.installer/src/org/graalvm/component/installer/Archive.java
@@ -30,4 +30,88 @@ import java.nio.channels.ReadableByteChannel;
 import org.graalvm.component.installer.model.ComponentInfo;
 
 /**
- * Simple a
+ * Simple abstraction over an archive, so that both JAR and RPMs (or other packaging) can be read.
+ * 
+ * @author sdedic
+ */
+public interface Archive extends Iterable<Archive.FileEntry>, AutoCloseable {
+    /**
+     * Opens an input stream for the entry.
+     * 
+     * @param e file entry
+     * @return the input stream
+     * @throws IOException on I/O error
+     */
+    InputStream getInputStream(FileEntry e) throws IOException;
+
+    /**
+     * Checks that contents of the entry matches the given file. If the archive stores checksums,
+     * the method may not need do byte comparison but can only compute checksum/hash on the supplied
+     * content and compare to archive's info.
+     * 
+     * @param bc the existing content
+     * @param entry archive entry
+     * @return true if the content is the same
+     * @throws IOException on I/O error
+     */
+    boolean checkContentsMatches(ReadableByteChannel bc, FileEntry entry) throws IOException;
+
+    /**
+     * Verifies integrity of the archive.
+     * 
+     * @param input options for verificaion
+     * @return true, if the archive has been verified
+     * @throws IOException
+     */
+    boolean verifyIntegrity(CommandInput input) throws IOException;
+
+    /**
+     * Completes metadata in `info' with information within the archive's contents. This method may
+     * need to iterate through files in the archive.
+     * 
+     * @param info
+     * @throws IOException
+     */
+    void completeMetadata(ComponentInfo info) throws IOException;
+
+    @Override
+    void close() throws IOException;
+
+    /**
+     * Represents a single entry in the archive.
+     */
+    interface FileEntry {
+        /**
+         * Returns name of the entry. Directory names should end with "/".
+         * 
+         * @return entry name
+         */
+        String getName();
+
+        /**
+         * @return true, if the entry represents a directory
+         */
+        boolean isDirectory();
+
+        /**
+         * True, if the entry is a symbolic link.
+         * 
+         * @return True, if the entry represents a symbolic link
+         */
+        boolean isSymbolicLink();
+
+        /**
+         * Link target for symbolic links.
+         * 
+         * @return target path
+         * @throws java.io.IOException if the link's target could not be read
+         * @throws IllegalStateException if the entry is not {@link #isSymbolicLink()}.
+         */
+        String getLinkTarget() throws IOException;
+
+        /**
+         * @return size of the content, only valid for regular files.
+         */
+        long getSize();
+    }
+}
