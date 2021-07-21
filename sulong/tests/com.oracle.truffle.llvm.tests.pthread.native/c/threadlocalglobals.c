d19c6773590987cba12f134c75d32467978e4b20
dutfile <113059452+dutfile@users.noreply.github.com>
2021-07-21 22:28:17 +0000

    * coding guidelines

--- sulong/tests/com.oracle.truffle.llvm.tests.pthread.native/c/threadlocalglobals.c
+++ sulong/tests/com.oracle.truffle.llvm.tests.pthread.native/c/threadlocalglobals.c
@@ -25,4 +25,31 @@
  * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * O
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *inc(void *ptr);
+
+__thread int j = 0;
+
+int main() {
+    pthread_t th1;
+    pthread_create(&th1, NULL, inc, NULL);
+    pthread_join(th1, NULL);
+    pthread_create(&th1, NULL, inc, NULL);
+    pthread_join(th1, NULL);
+    pthread_create(&th1, NULL, inc, NULL);
+    pthread_join(th1, NULL);
+    printf("now value is %d\n", j);
+    return 0;
+}
+
+void *inc(void *ptr) {
+    j++;
+    printf("thread %d\n", j);
+    return NULL;
+}
