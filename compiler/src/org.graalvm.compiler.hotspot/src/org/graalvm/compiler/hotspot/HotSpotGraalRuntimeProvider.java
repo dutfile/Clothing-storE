c33e65bfe2f89809ab038250d2f43acec996914d
dutfile <113059452+dutfile@users.noreply.github.com>
2021-09-03 09:13:56 +0000

    changed procfile

--- compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/HotSpotGraalRuntimeProvider.java
+++ compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/HotSpotGraalRuntimeProvider.java
@@ -18,4 +18,93 @@
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood 
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.compiler.hotspot;
+
+import java.io.PrintStream;
+import java.util.Map;
+
+import org.graalvm.compiler.api.runtime.GraalRuntime;
+import org.graalvm.compiler.core.CompilationWrapper.ExceptionAction;
+import org.graalvm.compiler.core.Instrumentation;
+import org.graalvm.compiler.core.common.CompilationIdentifier;
+import org.graalvm.compiler.debug.DebugContext;
+import org.graalvm.compiler.debug.DebugHandlersFactory;
+import org.graalvm.compiler.debug.DiagnosticsOutputDirectory;
+import org.graalvm.compiler.hotspot.meta.HotSpotProviders;
+import org.graalvm.compiler.options.OptionValues;
+import org.graalvm.compiler.replacements.SnippetCounter.Group;
+import org.graalvm.compiler.runtime.RuntimeProvider;
+
+import jdk.vm.ci.code.TargetDescription;
+
+//JaCoCo Exclude
+
+/**
+ * Configuration information for the HotSpot Graal runtime.
+ */
+public interface HotSpotGraalRuntimeProvider extends GraalRuntime, RuntimeProvider, Group.Factory {
+
+    default TargetDescription getTarget() {
+        return getHostBackend().getTarget();
+    }
+
+    HotSpotProviders getHostProviders();
+
+    @Override
+    default String getName() {
+        return getClass().getSimpleName();
+    }
+
+    HotSpotGraalRuntime.HotSpotGC getGarbageCollector();
+
+    @Override
+    HotSpotBackend getHostBackend();
+
+    GraalHotSpotVMConfig getVMConfig();
+
+    /**
+     * Opens a debug context for compiling {@code compilable}. The {@link DebugContext#close()}
+     * method should be called on the returned object once the compilation is finished.
+     *
+     * @param compilationOptions the options used to configure the compilation debug context
+     * @param compilationId a system wide unique compilation id
+     * @param compilable the input to the compilation
+     * @param logStream the log stream to use in this context
+     */
+    DebugContext openDebugContext(OptionValues compilationOptions, CompilationIdentifier compilationId, Object compilable, Iterable<DebugHandlersFactory> factories, PrintStream logStream);
+
+    /**
+     * Gets the option values associated with this runtime.
+     */
+    OptionValues getOptions();
+
+    /**
+     * Determines if the VM is currently bootstrapping the JVMCI compiler.
+     */
+    boolean isBootstrapping();
+
+    /**
+     * This runtime has been requested to shutdown.
+     */
+    boolean isShutdown();
+
+    /**
+     * Gets a directory into which diagnostics such crash reports and dumps should be written.
+     */
+    DiagnosticsOutputDirectory getOutputDirectory();
+
+    /**
+     * Gets the map used to count compilation problems at each {@link ExceptionAction} level. All
+     * updates and queries to the map should be synchronized.
+     */
+    Map<ExceptionAction, Integer> getCompilationProblemsPerAction();
+
+    /**
+     * Returns the instance holding the instrumentation data structures.
+     */
+    Instrumentation getInstrumentation();
+}
