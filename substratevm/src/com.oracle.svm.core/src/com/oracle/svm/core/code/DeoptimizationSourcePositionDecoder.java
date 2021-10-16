52b13ff3aaaafd068b940b8237afc4e64bae1e48
dutfile <113059452+dutfile@users.noreply.github.com>
2021-10-16 21:46:28 +0000

    Updated footer.html

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/code/DeoptimizationSourcePositionDecoder.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/code/DeoptimizationSourcePositionDecoder.java
@@ -28,4 +28,66 @@ import org.graalvm.compiler.graph.NodeSourcePosition;
 import org.graalvm.nativeimage.c.function.CodePointer;
 
 import com.oracle.svm.core.Uninterruptible;
-import com.oracle.svm.core.c.No
+import com.oracle.svm.core.c.NonmovableArray;
+import com.oracle.svm.core.c.NonmovableArrays;
+import com.oracle.svm.core.c.NonmovableObjectArray;
+import com.oracle.svm.core.util.NonmovableByteArrayTypeReader;
+
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+
+public class DeoptimizationSourcePositionDecoder {
+
+    static final int NO_SOURCE_POSITION = -1;
+    static final int NO_CALLER = 0;
+
+    @Uninterruptible(reason = "Must prevent the GC from freeing the CodeInfo object.")
+    public static NodeSourcePosition decode(int deoptId, CodePointer ip) {
+        UntetheredCodeInfo untetheredInfo = CodeInfoTable.lookupCodeInfo(ip);
+        if (untetheredInfo.isNull() || untetheredInfo.equal(CodeInfoTable.getImageCodeInfo())) {
+            /* We only have information for runtime compiled code, not for native image code. */
+            return null;
+        }
+
+        Object tether = CodeInfoAccess.acquireTether(untetheredInfo);
+        try {
+            CodeInfo info = CodeInfoAccess.convert(untetheredInfo, tether);
+            return decode0(deoptId, info);
+        } finally {
+            CodeInfoAccess.releaseTether(untetheredInfo, tether);
+        }
+    }
+
+    @Uninterruptible(reason = "Wrap the now safe call to interruptibly decode the source position.", calleeMustBe = false)
+    private static NodeSourcePosition decode0(int deoptId, CodeInfo info) {
+        return decode(deoptId, CodeInfoAccess.getDeoptimizationStartOffsets(info), CodeInfoAccess.getDeoptimizationEncodings(info), CodeInfoAccess.getDeoptimizationObjectConstants(info));
+    }
+
+    static NodeSourcePosition decode(int deoptId, NonmovableArray<Integer> deoptimizationStartOffsets,
+                    NonmovableArray<Byte> deoptimizationEncodings, NonmovableObjectArray<Object> deoptimizationObjectConstants) {
+        if (deoptId < 0 || deoptId >= NonmovableArrays.lengthOf(deoptimizationStartOffsets)) {
+            return null;
+        }
+
+        int startOffset = NonmovableArrays.getInt(deoptimizationStartOffsets, deoptId);
+        if (startOffset == NO_SOURCE_POSITION) {
+            return null;
+        }
+
+        NonmovableByteArrayTypeReader readBuffer = new NonmovableByteArrayTypeReader(deoptimizationEncodings, 0);
+        return decodeSourcePosition(startOffset, deoptimizationObjectConstants, readBuffer);
+    }
+
+    private static NodeSourcePosition decodeSourcePosition(long startOffset, NonmovableObjectArray<Object> deoptimizationObjectConstants, NonmovableByteArrayTypeReader readBuffer) {
+        readBuffer.setByteIndex(startOffset);
+        long callerRelativeOffset = readBuffer.getUV();
+        int bci = readBuffer.getSVInt();
+        ResolvedJavaMethod method = (ResolvedJavaMethod) NonmovableArrays.getObject(deoptimizationObjectConstants, readBuffer.getUVInt());
+
+        NodeSourcePosition caller = null;
+        if (callerRelativeOffset != NO_CALLER) {
+            caller = decodeSourcePosition(startOffset - callerRelativeOffset, deoptimizationObjectConstants, readBuffer);
+        }
+
+        return new NodeSourcePosition(caller, method, bci);
+    }
+}
