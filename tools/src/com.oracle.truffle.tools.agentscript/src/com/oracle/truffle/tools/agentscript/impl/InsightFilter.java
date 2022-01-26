d731e8d1c8dffc1f47f6f6ed5aca5172df0055e8
dutfile <113059452+dutfile@users.noreply.github.com>
2022-01-26 08:47:03 +0000

    bump changelogs

--- tools/src/com.oracle.truffle.tools.agentscript/src/com/oracle/truffle/tools/agentscript/impl/InsightFilter.java
+++ tools/src/com.oracle.truffle.tools.agentscript/src/com/oracle/truffle/tools/agentscript/impl/InsightFilter.java
@@ -161,3 +161,219 @@ final class InsightFilter {
     }
 
     int getLine() {
+        return line;
+    }
+
+    int getColumn() {
+        return column;
+    }
+
+    static InsightFilter.Data create(AgentType aType, Object[] arr)
+                    throws IllegalArgumentException, UnsupportedMessageException {
+        Set<Class<? extends Tag>> allTags = new HashSet<>();
+        String rootNameRegExp = null;
+        Object rootNameFn = null;
+        Object sourceFilterFn = null;
+        String sourcePathRegExp = null;
+        URI sourceURI = null;
+        int lineIs = 0;
+        int columnIs = 0;
+        if (arr != null && arr.length > 2) {
+            Object config = arr[2];
+            ObjectReader configReader = ObjectReader.get(config, CONFIG_PROPS);
+            if (configReader.is(CONFIG_EXPRESSIONS)) {
+                allTags.add(StandardTags.ExpressionTag.class);
+            }
+            if (configReader.is(CONFIG_STATEMENTS)) {
+                allTags.add(StandardTags.StatementTag.class);
+            }
+            if (configReader.is(CONFIG_ROOTS)) {
+                allTags.add(StandardTags.RootBodyTag.class);
+            }
+            Object rootNameFilter = configReader.get(CONFIG_ROOT_FILTER);
+            if (rootNameFilter != null) {
+                if (IOP.isString(rootNameFilter)) {
+                    rootNameRegExp = IOP.asString(rootNameFilter);
+                } else {
+                    if (!IOP.isExecutable(rootNameFilter)) {
+                        throw new IllegalArgumentException("rootNameFilter should be a string, a regular expression!");
+                    }
+                    rootNameFn = rootNameFilter;
+                }
+            }
+            Object sourceFilter = configReader.get(CONFIG_SOURCE_FILTER);
+            if (sourceFilter != null) {
+                if (!IOP.isExecutable(sourceFilter)) {
+                    throw new IllegalArgumentException("sourceFilter has to be a function!");
+                }
+                sourceFilterFn = sourceFilter;
+            }
+            Object at = configReader.get(CONFIG_AT);
+            if (at != null) {
+                ObjectReader atReader = ObjectReader.get(at, AT_PROPS);
+                Object sourcePath = atReader.get(AT_SOURCE_PATH);
+                Object sourceURIValue = atReader.get(AT_SOURCE_URI);
+                if (sourcePath != null && sourceURIValue != null) {
+                    throw new IllegalArgumentException("Both sourceURI and sourcePath is defined. Only one source specification is expected.");
+                }
+                if (sourcePath == null && sourceURIValue == null) {
+                    throw new IllegalArgumentException("Neither sourceURI nor sourcePath is defined. A source specification is expected.");
+                }
+                if (sourceURIValue != null) {
+                    if (!IOP.isString(sourceURIValue)) {
+                        throw new IllegalArgumentException("sourceURI is not a string.");
+                    }
+                    try {
+                        sourceURI = new URI(IOP.asString(sourceURIValue));
+                    } catch (URISyntaxException uex) {
+                        throw new IllegalArgumentException("sourceURI is not a valid URI: " + uex.getLocalizedMessage());
+                    }
+                }
+                if (sourcePath != null) {
+                    if (!IOP.isString(sourcePath)) {
+                        throw new IllegalArgumentException("sourcePath is not a string!");
+                    }
+                    sourcePathRegExp = IOP.asString(sourcePath);
+                }
+                Object lineValue = atReader.get(AT_LINE);
+                if (lineValue != null) {
+                    lineIs = getPositiveNumber(lineValue, "Source line");
+                }
+                Object columnValue = atReader.get(AT_COLUMN);
+                if (columnValue != null) {
+                    columnIs = getPositiveNumber(columnValue, "Source column");
+                }
+            }
+        }
+        if (allTags.isEmpty()) {
+            throw new IllegalArgumentException("No elements specified to listen to for execution listener. Need to specify at least one element kind: expressions, statements or roots.");
+        }
+
+        InsightFilter filter = new InsightFilter(allTags, rootNameRegExp, sourceURI, sourcePathRegExp, lineIs, columnIs, rootNameFn, sourceFilterFn);
+        return new Data(aType, filter, arr[1], rootNameFn, sourceFilterFn);
+    }
+
+    private static int getPositiveNumber(Object value, String errMsg) throws UnsupportedMessageException {
+        if (value == null) {
+            return 0;
+        }
+        if (IOP.fitsInInt(value)) {
+            int num = IOP.asInt(value);
+            if (num > 0) {
+                return num;
+            }
+        }
+        throw new IllegalArgumentException(errMsg + " is not a positive number or does not fit in integer type: " + IOP.asString(IOP.toDisplayString(value)));
+    }
+
+    private abstract static class ObjectReader {
+
+        static ObjectReader get(Object object, Set<String> properties) {
+            if (IOP.hasHashEntries(object)) {
+                return new HashObjectReader(object, properties);
+            } else {
+                return new MembersObjectReader(object);
+            }
+        }
+
+        boolean is(String propertyName) {
+            Object value = get(propertyName);
+            try {
+                return value != null && IOP.isBoolean(value) && IOP.asBoolean(value);
+            } catch (UnsupportedMessageException ex) {
+                throw CompilerDirectives.shouldNotReachHere();
+            }
+        }
+
+        abstract Object get(String propertyName);
+
+    }
+
+    private static class HashObjectReader extends ObjectReader {
+
+        private final EconomicMap<String, Object> map = EconomicMap.create();
+
+        HashObjectReader(Object object, Set<String> properties) {
+            try {
+                Object it = IOP.getHashEntriesIterator(object);
+                while (IOP.hasIteratorNextElement(it)) {
+                    try {
+                        Object keyAndValue = IOP.getIteratorNextElement(it);
+                        String name;
+                        Object value;
+                        try {
+                            Object key = IOP.readArrayElement(keyAndValue, 0);
+                            name = IOP.asString(key);
+                            if (!properties.contains(name)) {
+                                throw InsightException.unknownAttribute(name);
+                            }
+                            value = IOP.readArrayElement(keyAndValue, 1);
+                        } catch (InvalidArrayIndexException ex) {
+                            throw CompilerDirectives.shouldNotReachHere(ex);
+                        }
+                        if (!IOP.isNull(value)) {
+                            map.put(name, value);
+                        }
+                    } catch (StopIterationException ex) {
+                        break;
+                    }
+                }
+            } catch (UnsupportedMessageException ex) {
+                throw CompilerDirectives.shouldNotReachHere();
+            }
+        }
+
+        @Override
+        Object get(String propertyName) {
+            return map.get(propertyName);
+        }
+    }
+
+    private static class MembersObjectReader extends ObjectReader {
+
+        private final Object object;
+
+        MembersObjectReader(Object object) {
+            if (!IOP.hasMembers(object)) {
+                try {
+                    throw new IllegalArgumentException("Config object " + IOP.asString(IOP.toDisplayString(object)) + " does not have members.");
+                } catch (UnsupportedMessageException ex) {
+                    throw CompilerDirectives.shouldNotReachHere();
+                }
+            }
+            this.object = object;
+        }
+
+        @Override
+        Object get(String propertyName) {
+            Object value;
+            try {
+                value = IOP.readMember(object, propertyName);
+                if (value != null && IOP.isNull(value)) {
+                    value = null;
+                }
+            } catch (UnknownIdentifierException ex) {
+                value = null;
+            } catch (UnsupportedMessageException ex) {
+                throw CompilerDirectives.shouldNotReachHere();
+            }
+            return value;
+        }
+    }
+
+    public static final class Data {
+        final AgentType type;
+        final InsightFilter filter;
+        final Object fn;
+        final Object rootNameFn;
+        final Object sourceFilterFn;
+
+        private Data(AgentType type, InsightFilter filter, Object fn, Object rootNameFn, Object sourceFilterFn) {
+            this.type = type;
+            this.filter = filter;
+            this.fn = fn;
+            this.rootNameFn = rootNameFn;
+            this.sourceFilterFn = sourceFilterFn;
+        }
+    }
+}
