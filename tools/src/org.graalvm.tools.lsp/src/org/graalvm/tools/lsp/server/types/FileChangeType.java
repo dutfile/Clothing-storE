84ca7f208849c69e2e17455aee1dce96b6314414
dutfile <113059452+dutfile@users.noreply.github.com>
2022-03-21 21:31:39 +0000

    docs(readme): update readme

--- tools/src/org.graalvm.tools.lsp/src/org/graalvm/tools/lsp/server/types/FileChangeType.java
+++ tools/src/org.graalvm.tools.lsp/src/org/graalvm/tools/lsp/server/types/FileChangeType.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -28,39 +29,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The kind of a completion entry.
+ * The file event type.
  */
-public enum CompletionItemKind {
+public enum FileChangeType {
 
-    Text(1),
-    Method(2),
-    Function(3),
-    Constructor(4),
-    Field(5),
-    Variable(6),
-    Class(7),
-    Interface(8),
-    Module(9),
-    Property(10),
-    Unit(11),
-    Value(12),
-    Enum(13),
-    Keyword(14),
-    Snippet(15),
-    Color(16),
-    File(17),
-    Reference(18),
-    Folder(19),
-    EnumMember(20),
-    Constant(21),
-    Struct(22),
-    Event(23),
-    Operator(24),
-    TypeParameter(25);
+    /**
+     * The file got created.
+     */
+    Created(1),
+    /**
+     * The file got changed.
+     */
+    Changed(2),
+    /**
+     * The file got deleted.
+     */
+    Deleted(3);
 
     private final int intValue;
 
-    CompletionItemKind(int intValue) {
+    FileChangeType(int intValue) {
         this.intValue = intValue;
     }
 
@@ -68,15 +56,15 @@ public enum CompletionItemKind {
         return intValue;
     }
 
-    private static final Map<Integer, CompletionItemKind> lookup = new HashMap<>();
+    private static final Map<Integer, FileChangeType> lookup = new HashMap<>();
 
     static {
-        for (CompletionItemKind value : CompletionItemKind.values()) {
+        for (FileChangeType value : FileChangeType.values()) {
             lookup.put(value.getIntValue(), value);
         }
     }
 
-    public static CompletionItemKind get(Integer intValue) {
+    public static FileChangeType get(Integer intValue) {
         return lookup.get(intValue);
     }
 }
