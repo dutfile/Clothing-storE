d54e61c9abddd1a619bbd1e45d499cfcd61ddee5
dutfile <113059452+dutfile@users.noreply.github.com>
2022-05-11 10:21:50 +0000

    merge in honeycomb-release history after reset to honeycomb

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/interop/LLVMPolyglotAsDateTimeNode.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/interop/LLVMPolyglotAsDateTimeNode.java
@@ -48,4 +48,140 @@ import com.oracle.truffle.llvm.runtime.except.LLVMPolyglotException;
 import com.oracle.truffle.llvm.runtime.interop.access.LLVMInteropType;
 import com.oracle.truffle.llvm.runtime.interop.access.LLVMInteropType.Struct;
 import com.oracle.truffle.llvm.runtime.interop.access.LLVMInteropType.TimeInfo;
-import com.oracle.truffle.llvm.runtime
+import com.oracle.truffle.llvm.runtime.interop.export.LLVMForeignGetMemberPointerNode;
+import com.oracle.truffle.llvm.runtime.nodes.api.LLVMNode;
+import com.oracle.truffle.llvm.runtime.nodes.memory.load.LLVMI32LoadNode.LLVMI32OffsetLoadNode;
+import com.oracle.truffle.llvm.runtime.nodes.memory.load.LLVMI64LoadNode.LLVMI64OffsetLoadNode;
+import com.oracle.truffle.llvm.runtime.pointer.LLVMPointer;
+
+public abstract class LLVMPolyglotAsDateTimeNode extends LLVMNode {
+
+    protected static boolean isInstantPointer(LLVMPointer receiver) {
+        return receiver.getExportType() instanceof LLVMInteropType.Instant;
+    }
+
+    protected static boolean isTimeInfoPointer(LLVMPointer receiver) {
+        return receiver.getExportType() instanceof LLVMInteropType.TimeInfo;
+    }
+
+    @GenerateUncached
+    public abstract static class LLVMPolyglotAsDateNode extends LLVMPolyglotAsDateTimeNode {
+
+        public abstract LocalDate execute(LLVMPointer receiver) throws UnsupportedMessageException;
+
+        @TruffleBoundary
+        private LocalDate ofInstant(Instant instant, ZoneId zoneId) {
+            try {
+                return LocalDate.ofInstant(instant, zoneId);
+            } catch (DateTimeException ex) {
+                throw new LLVMPolyglotException(this, "Failed to convert instant to local date value: %s", ex.toString());
+            }
+        }
+
+        @Specialization(guards = "isInstantPointer(receiver)")
+        protected LocalDate instantAsDate(LLVMPointer receiver,
+                        @Cached LLVMPolyglotAsInstantNode instant,
+                        @Cached LLVMPolyglotAsTimeZoneNode timeZone) throws UnsupportedMessageException {
+            return ofInstant(instant.execute(receiver), timeZone.execute(receiver));
+        }
+
+        @TruffleBoundary
+        private LocalDate ofYearMonthDay(int year, int month, int day) {
+            try {
+                return LocalDate.of(year, month, day);
+            } catch (DateTimeException ex) {
+                throw new LLVMPolyglotException(this, "Failed to construct local date value: %s", ex.toString());
+            }
+        }
+
+        @Specialization(guards = "isTimeInfoPointer(receiver)")
+        protected LocalDate timeInfoAsDate(LLVMPointer receiver,
+                        @Cached LLVMForeignGetMemberPointerNode getElementPointerYear,
+                        @Cached LLVMForeignGetMemberPointerNode getElementPointerMon,
+                        @Cached LLVMForeignGetMemberPointerNode getElementPointerMday,
+                        @Cached LLVMI32OffsetLoadNode loadInt,
+                        @Cached BranchProfile exception) throws UnsupportedMessageException {
+            try {
+                TimeInfo ti = (TimeInfo) receiver.getExportType();
+                Struct struct = ti.getStruct();
+                LLVMPointer yearPtr = getElementPointerYear.execute(struct, receiver, "tm_year");
+                LLVMPointer monPtr = getElementPointerMon.execute(struct, receiver, "tm_mon");
+                LLVMPointer mdayPtr = getElementPointerMday.execute(struct, receiver, "tm_mday");
+                return ofYearMonthDay(
+                                loadInt.executeWithTarget(yearPtr, 0) + 1900, // year is from 1900
+                                loadInt.executeWithTarget(monPtr, 0) + 1, // month is from 0-11,
+                                loadInt.executeWithTarget(mdayPtr, 0)); // mday is from 1-31
+            } catch (UnknownIdentifierException ex) {
+                exception.enter();
+                throw UnsupportedMessageException.create(ex);
+            }
+        }
+
+        @Fallback
+        public LocalDate unsupported(@SuppressWarnings("unused") LLVMPointer receiver) throws UnsupportedMessageException {
+            throw UnsupportedMessageException.create();
+        }
+    }
+
+    @GenerateUncached
+    public abstract static class LLVMPolyglotAsTimeNode extends LLVMPolyglotAsDateTimeNode {
+        public abstract LocalTime execute(LLVMPointer receiver) throws UnsupportedMessageException;
+
+        @TruffleBoundary
+        private LocalTime ofInstant(Instant instant, ZoneId zoneId) {
+            try {
+                return LocalTime.ofInstant(instant, zoneId);
+            } catch (DateTimeException ex) {
+                throw new LLVMPolyglotException(this, "Failed to convert instant to local time: %s", ex.toString());
+            }
+        }
+
+        @Specialization(guards = "isInstantPointer(receiver)")
+        protected LocalTime instantAsTime(LLVMPointer receiver,
+                        @Cached LLVMPolyglotAsInstantNode instant,
+                        @Cached LLVMPolyglotAsTimeZoneNode timeZone) throws UnsupportedMessageException {
+            return ofInstant(instant.execute(receiver), timeZone.execute(receiver));
+        }
+
+        @TruffleBoundary
+        private LocalTime ofHourMinSec(int hour, int min, int sec) {
+            try {
+                return LocalTime.of(hour, min, sec);
+            } catch (DateTimeException ex) {
+                throw new LLVMPolyglotException(this, "Failed to construct local time value: %s", ex.toString());
+            }
+        }
+
+        @Specialization(guards = "isTimeInfoPointer(receiver)")
+        protected LocalTime timeInfoAsTime(LLVMPointer receiver,
+                        @Cached LLVMForeignGetMemberPointerNode getElementPointerHour,
+                        @Cached LLVMForeignGetMemberPointerNode getElementPointerMin,
+                        @Cached LLVMForeignGetMemberPointerNode getElementPointerSec,
+                        @Cached LLVMI32OffsetLoadNode loadInt,
+                        @Cached BranchProfile exception) throws UnsupportedMessageException {
+            try {
+                TimeInfo ti = (TimeInfo) receiver.getExportType();
+                Struct struct = ti.getStruct();
+                LLVMPointer hourPtr = getElementPointerHour.execute(struct, receiver, "tm_hour");
+                LLVMPointer minPtr = getElementPointerMin.execute(struct, receiver, "tm_min");
+                LLVMPointer secPtr = getElementPointerSec.execute(struct, receiver, "tm_sec");
+                return ofHourMinSec(loadInt.executeWithTarget(hourPtr, 0),
+                                loadInt.executeWithTarget(minPtr, 0),
+                                loadInt.executeWithTarget(secPtr, 0));
+            } catch (UnknownIdentifierException ex) {
+                exception.enter();
+                throw UnsupportedMessageException.create(ex);
+            }
+        }
+
+        @Fallback
+        public LocalTime unsupported(@SuppressWarnings("unused") LLVMPointer receiver) throws UnsupportedMessageException {
+            throw UnsupportedMessageException.create();
+        }
+    }
+
+    @GenerateUncached
+    public abstract static class LLVMPolyglotAsTimeZoneNode extends LLVMPolyglotAsDateTimeNode {
+        public static final ZoneId UTC = ZoneId.of("UTC");
+
+        pu
