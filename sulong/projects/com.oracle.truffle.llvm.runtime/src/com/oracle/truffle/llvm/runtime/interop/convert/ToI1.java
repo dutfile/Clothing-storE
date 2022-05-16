22687587413de64cd9b9bb528c4a2e6d733e02e7
dutfile <113059452+dutfile@users.noreply.github.com>
2022-05-16 11:21:37 +0000

    Update command.js

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/convert/ToI1.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/convert/ToI1.java
@@ -39,4 +39,86 @@ import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.profiles.BranchProfile;
 import com.oracle.truffle.llvm.runtime.except.LLVMPolyglotException;
-import com.oracle.truffle.llvm.runtime
+import com.oracle.truffle.llvm.runtime.library.internal.LLVMAsForeignLibrary;
+
+public abstract class ToI1 extends ForeignToLLVM {
+
+    @Specialization
+    protected boolean fromInt(int value) {
+        return value != 0;
+    }
+
+    @Specialization
+    protected boolean fromChar(char value) {
+        return value != 0;
+    }
+
+    @Specialization
+    protected boolean fromShort(short value) {
+        return value != 0;
+    }
+
+    @Specialization
+    protected boolean fromLong(long value) {
+        return value != 0;
+    }
+
+    @Specialization
+    protected boolean fromByte(byte value) {
+        return value != 0;
+    }
+
+    @Specialization
+    protected boolean fromFloat(float value) {
+        return value != 0;
+    }
+
+    @Specialization
+    protected boolean fromDouble(double value) {
+        return value != 0;
+    }
+
+    @Specialization
+    protected boolean fromBoolean(boolean value) {
+        return value;
+    }
+
+    @Specialization
+    protected boolean fromString(String value,
+                    @Cached BranchProfile exception) {
+        return getSingleStringCharacter(value, exception) != 0;
+    }
+
+    @Specialization(limit = "5", guards = {"foreigns.isForeign(obj)", "interop.isBoolean(foreigns.asForeign(obj))"})
+    @GenerateAOT.Exclude
+    protected boolean fromForeign(Object obj,
+                    @CachedLibrary("obj") LLVMAsForeignLibrary foreigns,
+                    @CachedLibrary(limit = "3") InteropLibrary interop,
+                    @Cached BranchProfile exception) {
+        try {
+            return interop.asBoolean(foreigns.asForeign(obj));
+        } catch (UnsupportedMessageException ex) {
+            exception.enter();
+            throw new LLVMPolyglotException(this, "Foreign object can't be converted to boolean.");
+        }
+    }
+
+    @TruffleBoundary
+    static boolean slowPathPrimitiveConvert(ForeignToLLVM thiz, Object value) throws UnsupportedTypeException {
+        if (value instanceof Number) {
+            return ((Number) value).longValue() != 0;
+        } else if (value instanceof Boolean) {
+            return (boolean) value;
+        } else if (value instanceof Character) {
+            return (char) value != 0;
+        } else if (value instanceof String) {
+            return thiz.getSingleStringCharacter((String) value, BranchProfile.getUncached()) != 0;
+        } else {
+            try {
+                return InteropLibrary.getFactory().getUncached().asBoolean(value);
+            } catch (UnsupportedMessageException ex) {
+                throw UnsupportedTypeException.create(new Object[]{value});
+            }
+        }
+    }
+}
