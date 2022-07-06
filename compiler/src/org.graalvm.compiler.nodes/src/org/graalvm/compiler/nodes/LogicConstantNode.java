c6bddeaadd6794706a9ed1be79eadab23267773f
dutfile <113059452+dutfile@users.noreply.github.com>
2022-07-06 15:54:27 +0000

    Test Coverage

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/LogicConstantNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/LogicConstantNode.java
@@ -36,4 +36,66 @@ import org.graalvm.compiler.nodes.spi.LIRLowerable;
  * The {@code LogicConstantNode} represents a boolean constant.
  */
 @NodeInfo(nameTemplate = "{p#value}", cycles = CYCLES_0, size = SIZE_0)
-public f
+public final class LogicConstantNode extends LIRLowerableLogicNode implements LIRLowerable {
+
+    public static final NodeClass<LogicConstantNode> TYPE = NodeClass.create(LogicConstantNode.class);
+    protected final boolean value;
+
+    public LogicConstantNode(boolean value) {
+        super(TYPE);
+        this.value = value;
+    }
+
+    /**
+     * Returns a node for a boolean constant.
+     *
+     * @param v the boolean value for which to create the instruction
+     * @param graph
+     * @return a node representing the boolean
+     */
+    public static LogicConstantNode forBoolean(boolean v, Graph graph) {
+        return graph.unique(new LogicConstantNode(v));
+    }
+
+    /**
+     * Returns a node for a boolean constant.
+     *
+     * @param v the boolean value for which to create the instruction
+     * @return a node representing the boolean
+     */
+    public static LogicConstantNode forBoolean(boolean v) {
+        return new LogicConstantNode(v);
+    }
+
+    /**
+     * Gets a constant for {@code true}.
+     */
+    public static LogicConstantNode tautology(Graph graph) {
+        return forBoolean(true, graph);
+    }
+
+    /**
+     * Gets a constant for {@code false}.
+     */
+    public static LogicConstantNode contradiction(Graph graph) {
+        return forBoolean(false, graph);
+    }
+
+    /**
+     * Gets a constant for {@code true}.
+     */
+    public static LogicConstantNode tautology() {
+        return forBoolean(true);
+    }
+
+    /**
+     * Gets a constant for {@code false}.
+     */
+    public static LogicConstantNode contradiction() {
+        return forBoolean(false);
+    }
+
+    public boolean getValue() {
+        return value;
+    }
+}
