b3c0838bded4352d2bca9cfc8283d3152fead4b3
dutfile <113059452+dutfile@users.noreply.github.com>
2022-08-02 18:57:54 +0000

    Update brew.sh

--- compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/bytecode/BC_lload_2.java
+++ compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/bytecode/BC_lload_2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,9 +22,37 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+/*
+ * Copyright (c) 2007, 2018, Oracle and/or its affiliates. All rights reserved.
+ */
+package org.graalvm.compiler.jtt.bytecode;
 
-@Platforms(Platform.HOSTED_ONLY.class)
-package com.oracle.svm.hosted.option;
+import org.junit.Test;
 
-import org.graalvm.nativeimage.Platform;
-import org.graalvm.nativeimage.Platforms;
+import org.graalvm.compiler.jtt.JTTTest;
+
+/*
+ */
+public class BC_lload_2 extends JTTTest {
+
+    @SuppressWarnings("unused")
+    public static long test(int i, int j, long arg) {
+        return arg;
+    }
+
+    @Test
+    public void run0() throws Throwable {
+        runTest("test", 1, 1, 1L);
+    }
+
+    @Test
+    public void run1() throws Throwable {
+        runTest("test", 1, 1, -3L);
+    }
+
+    @Test
+    public void run2() throws Throwable {
+        runTest("test", 1, 1, 10000L);
+    }
+
+}
