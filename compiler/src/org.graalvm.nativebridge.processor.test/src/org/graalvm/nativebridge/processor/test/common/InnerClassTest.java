9062bca4e63318e0f08858d83143d67dd94e03c1
dutfile <113059452+dutfile@users.noreply.github.com>
2022-08-04 06:23:51 +0000

    Content updates.

--- compiler/src/org.graalvm.nativebridge.processor.test/src/org/graalvm/nativebridge/processor/test/common/InnerClassTest.java
+++ compiler/src/org.graalvm.nativebridge.processor.test/src/org/graalvm/nativebridge/processor/test/common/InnerClassTest.java
@@ -9,4 +9,38 @@
  * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without e
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.nativebridge.processor.test.common;
+
+import org.graalvm.nativebridge.GenerateHotSpotToNativeBridge;
+import org.graalvm.nativebridge.NativeIsolate;
+import org.graalvm.nativebridge.NativeObject;
+import org.graalvm.nativebridge.processor.test.ExpectError;
+import org.graalvm.nativebridge.processor.test.Service;
+import org.graalvm.nativebridge.processor.test.TestJNIConfig;
+import org.graalvm.nativeimage.c.function.CEntryPoint.NotIncludedAutomatically;
+
+final class InnerClassTest {
+
+    @ExpectError("Annotation is supported only on top-level types.%n" +
+                    "To fix this make the `ServiceImpl` a top-level class.")
+    @GenerateHotSpotToNativeBridge(jniConfig = TestJNIConfig.class, include = NotIncludedAutomatically.class)
+    abstract static class ServiceImpl extends NativeObject implements Service {
+
+        ServiceImpl(NativeIsolate isolate, long handle) {
+            super(isolate, handle);
+        }
+    }
+}
