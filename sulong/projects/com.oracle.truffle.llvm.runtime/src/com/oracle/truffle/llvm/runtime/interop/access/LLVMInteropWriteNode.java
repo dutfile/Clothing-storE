ea55b752d1be785c73d4caf93ebfa8c3647a3b9a
dutfile <113059452+dutfile@users.noreply.github.com>
2022-08-28 17:44:34 +0000

    rwsem: do not block readers at head of queue if other readers are active

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropWriteNode.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropWriteNode.java
@@ -33,4 +33,123 @@ import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GenerateAOT;
-import com.orac
+import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.ImportStatic;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.InvalidArrayIndexException;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.interop.UnsupportedTypeException;
+import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.profiles.BranchProfile;
+import com.oracle.truffle.llvm.runtime.except.LLVMPolyglotException;
+import com.oracle.truffle.llvm.runtime.interop.LLVMDataEscapeNode;
+import com.oracle.truffle.llvm.runtime.interop.access.LLVMInteropAccessNode.AccessLocation;
+import com.oracle.truffle.llvm.runtime.interop.convert.ForeignToLLVM.ForeignToLLVMType;
+import com.oracle.truffle.llvm.runtime.nodes.api.LLVMNode;
+
+@GenerateUncached
+public abstract class LLVMInteropWriteNode extends LLVMNode {
+
+    public static LLVMInteropWriteNode create() {
+        return LLVMInteropWriteNodeGen.create();
+    }
+
+    public abstract void execute(LLVMInteropType.Structured baseType, Object foreign, long offset, Object value, ForeignToLLVMType writeType);
+
+    @Specialization(guards = "type != null")
+    void doKnownType(LLVMInteropType.Structured type, Object foreign, long offset, Object value, ForeignToLLVMType writeType,
+                    @Cached LLVMInteropAccessNode access,
+                    @Cached WriteLocationNode write) {
+        AccessLocation location = access.execute(type, foreign, offset);
+        write.execute(location.identifier, location, value, writeType);
+    }
+
+    @Specialization(guards = "type == null")
+    void doUnknownType(@SuppressWarnings("unused") LLVMInteropType.Structured type, Object foreign, long offset, Object value, ForeignToLLVMType writeType,
+                    @Cached WriteLocationNode write) {
+        // type unknown: fall back to "array of unknown value type"
+        AccessLocation location = new AccessLocation(foreign, Long.divideUnsigned(offset, writeType.getSizeInBytes()), null);
+        write.execute(location.identifier, location, value, writeType);
+    }
+
+    @GenerateUncached
+    abstract static class WriteLocationNode extends LLVMNode {
+
+        abstract void execute(Object identifier, AccessLocation location, Object value, ForeignToLLVMType writeType);
+
+        @Specialization(limit = "3")
+        @GenerateAOT.Exclude
+        void writeMember(String identifier, AccessLocation location, Object value, @SuppressWarnings("unused") ForeignToLLVMType writeType,
+                        @CachedLibrary("location.base") InteropLibrary interop,
+                        @Cached ConvertOutgoingNode convertOutgoing,
+                        @Cached BranchProfile exception) {
+            assert identifier == location.identifier;
+            try {
+                interop.writeMember(location.base, identifier, convertOutgoing.execute(value, location.type, writeType));
+            } catch (UnsupportedMessageException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Cannot write member '%s'.", identifier);
+            } catch (UnknownIdentifierException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Member '%s' not found.", identifier);
+            } catch (UnsupportedTypeException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Wrong type writing to member '%s'.", identifier);
+            }
+        }
+
+        @Specialization(guards = "isLocationTypeNullOrSameSize(location, writeType)", limit = "3")
+        @GenerateAOT.Exclude
+        void writeArrayElementTypeMatch(long identifier, AccessLocation location, Object value, ForeignToLLVMType writeType,
+                        @CachedLibrary("location.base") InteropLibrary interop,
+                        @Cached ConvertOutgoingNode convertOutgoing,
+                        @Cached BranchProfile exception) {
+            assert identifier == (Long) location.identifier;
+            long idx = identifier;
+            try {
+                interop.writeArrayElement(location.base, idx, convertOutgoing.execute(value, location.type, writeType));
+            } catch (InvalidArrayIndexException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Invalid array index %d.", idx);
+            } catch (UnsupportedMessageException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Cannot write array element %d.", idx);
+            } catch (UnsupportedTypeException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Wrong type writing to array element %d.", idx);
+            }
+        }
+
+        @Specialization(guards = {"!isLocationTypeNullOrSameSize(location, writeType)", "locationType.isI8()", "writeTypeSizeInBytes > 1"}, limit = "3")
+        @GenerateAOT.Exclude
+        void writeArrayElementToI8(long identifier, AccessLocation location, Object value, ForeignToLLVMType writeType,
+                        @CachedLibrary("location.base") InteropLibrary interop,
+                        @Cached ReinterpretLLVMAsLong toLong,
+                        @Cached BranchProfile exception,
+                        @Bind("location.type.kind.foreignToLLVMType") @SuppressWarnings("unused") ForeignToLLVMType locationType,
+                        @Bind("writeType.getSizeInBytes()") int writeTypeSizeInBytes) {
+            assert identifier == (Long) location.identifier;
+            long idx = identifier;
+            try {
+                long longValue = toLong.executeWithWriteType(value, writeType);
+                // TODO (je) this currently assumes little endian (GR-24919)
+                for (int i = 0; i < writeTypeSizeInBytes; i++, idx++) {
+                    interop.writeArrayElement(location.base, idx, (byte) longValue);
+                    longValue >>= 8;
+                }
+            } catch (InvalidArrayIndexException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Invalid array index %d.", idx);
+            } catch (UnsupportedMessageException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Cannot write array element %d.", idx);
+            } catch (UnsupportedTypeException ex) {
+                exception.enter();
+                throw new LLVMPolyglotException(this, "Wrong type writing to array element %d.", idx);
+            }
+        }
+
+        @Fallback
+        void fallback(@SuppressWarnings("unused") Object identifier, AccessLoca
