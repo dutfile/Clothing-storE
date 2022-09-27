60102c6d14df3d6c32577748b20d07bf26c89e0e
dutfile <113059452+dutfile@users.noreply.github.com>
2022-09-27 08:46:07 +0000

    I dunno

--- compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/VerifySnippetProbabilities.java
+++ compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/VerifySnippetProbabilities.java
@@ -113,3 +113,74 @@ public class VerifySnippetProbabilities extends VerifyPhase<CoreProviders> {
                             Invoke invoke = (Invoke) input;
                             CallTargetNode mc = invoke.callTarget();
                             if (mc.invokeKind().isDirect()) {
+                                ResolvedJavaMethod targetMethod = mc.targetMethod();
+                                if (knownIntrinsicMethods.contains(targetMethod)) {
+                                    found = true;
+                                    break;
+                                } else if (targetMethod.getAnnotation(Fold.class) != null) {
+                                    // will fold the entire if away, not necessary to verify
+                                    found = true;
+                                    break;
+                                } else {
+                                    // some snippets have complex semantics separated in different
+                                    // method in the same class, allow such patterns they will be
+                                    // fully inlined
+                                    if (targetMethod.getDeclaringClass().equals(method.getDeclaringClass())) {
+                                        found = true;
+                                        break;
+                                    }
+                                    // reading folded options in snippets may require some unboxing
+                                    for (Node argument : mc.arguments()) {
+                                        ValueNode pureArg = GraphUtil.unproxify((ValueNode) argument);
+                                        if (pureArg instanceof Invoke && ((Invoke) pureArg).getTargetMethod().getAnnotation(Fold.class) != null) {
+                                            found = true;
+                                            break outer;
+                                        }
+                                    }
+                                }
+                            } else {
+                                // abstract / interface methods called in a snippet, most likely due
+                                // to overriden snippet logic that folds later, ignore
+                                found = true;
+                                break;
+                            }
+                        } else if (input instanceof ParameterNode) {
+                            if (specialParameters[((ParameterNode) input).index()]) {
+                                // constant or non null parameter, ignore
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!found) {
+                        throw new VerificationError("Node %s in snippet %s has unknown probability %s (nsp %s) and does not call" +
+                                        "BranchProbabilityNode.probabiliy/GraalDirectives.inject<> on any of it's condition inputs.",
+                                        ifNode, graph, profile,
+                                        ifNode.getNodeSourcePosition());
+
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean isExplodedLoopExit(IfNode ifNode) {
+        LoopExitNode lex = null;
+        if (ifNode.trueSuccessor() instanceof LoopExitNode) {
+            lex = (LoopExitNode) ifNode.trueSuccessor();
+        } else if (ifNode.falseSuccessor() instanceof LoopExitNode) {
+            lex = (LoopExitNode) ifNode.falseSuccessor();
+        }
+        if (lex != null) {
+            for (FixedNode f : GraphUtil.predecessorIterable(lex.loopBegin().forwardEnd())) {
+                if (f instanceof Invoke) {
+                    if (((Invoke) f).callTarget().targetMethod().getName().equals("explodeLoop")) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
