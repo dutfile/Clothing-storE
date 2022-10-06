d859c7e98ebb69380f0b91fa2f04d99de192b27c
dutfile <113059452+dutfile@users.noreply.github.com>
2022-10-06 22:38:08 +0000

    Initial tryout

--- visualizer/IdealGraphVisualizer/GraphSearch/src/org/graalvm/visualizer/search/ui/SearchResultsEntry.java
+++ visualizer/IdealGraphVisualizer/GraphSearch/src/org/graalvm/visualizer/search/ui/SearchResultsEntry.java
@@ -12,4 +12,46 @@
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
  *
- * You should have recei
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.visualizer.search.ui;
+
+import javax.swing.JPanel;
+import org.openide.util.Lookup;
+
+/**
+ * Interface between SearchREsults tab and the container.
+ * @author sdedic
+ */
+public interface SearchResultsEntry {
+    /**
+     * @return Returns Lookup of the Search Results. Should reflect the selected nodes.
+     */
+    public Lookup getLookup();
+    
+    /**
+     * @return the actual UI to be embedded into the result view
+     */
+    public JPanel getResultsPanel();
+    
+    /**
+     * Cancels the search, if pending
+     */
+    public void cancelSearch();
+    
+    /**
+     * Informs the view was opened.
+     */
+    public void viewOpened();
+    
+    /**
+     * Informs the view was closed.
+     */
+    public void viewClosed();
+}
