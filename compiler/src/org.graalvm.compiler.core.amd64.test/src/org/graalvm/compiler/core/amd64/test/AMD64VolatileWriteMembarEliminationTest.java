c23ab33a0ebd61c87e716d76eeb2553d7d8b33e7
dutfile <113059452+dutfile@users.noreply.github.com>
2022-10-11 20:59:23 +0000

    update folder

--- compiler/src/org.graalvm.compiler.core.amd64.test/src/org/graalvm/compiler/core/amd64/test/AMD64VolatileWriteMembarEliminationTest.java
+++ compiler/src/org.graalvm.compiler.core.amd64.test/src/org/graalvm/compiler/core/amd64/test/AMD64VolatileWriteMembarEliminationTest.java
@@ -18,4 +18,93 @@
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shor
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.compiler.core.amd64.test;
+
+import static org.junit.Assume.assumeTrue;
+
+import org.graalvm.compiler.core.common.memory.MemoryOrderMode;
+import org.graalvm.compiler.core.test.GraalCompilerTest;
+import org.graalvm.compiler.nodes.StructuredGraph;
+import org.graalvm.compiler.nodes.memory.WriteNode;
+import org.junit.Before;
+import org.junit.Test;
+
+import jdk.vm.ci.amd64.AMD64;
+
+public class AMD64VolatileWriteMembarEliminationTest extends GraalCompilerTest {
+    static Object[] objectArray = new Object[3];
+    static int field1;
+    static int field2;
+
+    static {
+        objectArrayBaseOffset = UNSAFE.arrayBaseOffset(Object[].class);
+        objectArrayIndexScale = UNSAFE.arrayIndexScale(Object[].class);
+    }
+
+    private static final long objectArrayBaseOffset;
+    private static final long objectArrayIndexScale;
+
+    @Before
+    public void checkAMD64() {
+        assumeTrue("skipping AMD64 specific test", getTarget().arch instanceof AMD64);
+    }
+
+    public static void testMethod1() {
+        Object[] array = objectArray;
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset, array);
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset + (1 * objectArrayIndexScale), array);
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset + (2 * objectArrayIndexScale), array);
+    }
+
+    static int expectedVolatiles;
+
+    @Test
+    public void test1() {
+        expectedVolatiles = 1;
+        test("testMethod1");
+    }
+
+    @Override
+    protected void checkLowTierGraph(StructuredGraph graph) {
+        assertDeepEquals("Wrong number of membars", expectedVolatiles,
+                        graph.getNodes().filter(WriteNode.class).filter(n -> ((WriteNode) n).getMemoryOrder() == MemoryOrderMode.VOLATILE).count());
+    }
+
+    public static int testMethod2() {
+        int result = 0;
+        Object[] array = objectArray;
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset, array);
+        result += field1;
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset + (1 * objectArrayIndexScale), array);
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset + (2 * objectArrayIndexScale), array);
+        return result;
+    }
+
+    @Test
+    public void test2() {
+        expectedVolatiles = 2;
+        test("testMethod2");
+    }
+
+    public static int testMethod3() {
+        int result = 0;
+        Object[] array = objectArray;
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset, array);
+        field1 = 1;
+        field2 = 2;
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset + (1 * objectArrayIndexScale), array);
+        UNSAFE.putObjectVolatile(array, objectArrayBaseOffset + (2 * objectArrayIndexScale), array);
+        return result;
+    }
+
+    @Test
+    public void test3() {
+        expectedVolatiles = 1;
+        test("testMethod3");
+    }
+
+}
