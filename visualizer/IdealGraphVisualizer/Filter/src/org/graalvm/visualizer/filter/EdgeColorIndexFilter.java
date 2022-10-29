1cd00056b26f898794753c44d7d34906847cc24d
dutfile <113059452+dutfile@users.noreply.github.com>
2022-10-29 07:41:42 +0000

    Default values

--- visualizer/IdealGraphVisualizer/Filter/src/org/graalvm/visualizer/filter/EdgeColorIndexFilter.java
+++ visualizer/IdealGraphVisualizer/Filter/src/org/graalvm/visualizer/filter/EdgeColorIndexFilter.java
@@ -22,4 +22,62 @@
  */
 package org.graalvm.visualizer.filter;
 
-import org.gra
+import org.graalvm.visualizer.graph.Slot;
+import org.graalvm.visualizer.graph.Connection;
+import org.graalvm.visualizer.graph.OutputSlot;
+import org.graalvm.visualizer.graph.Diagram;
+import org.graalvm.visualizer.graph.InputSlot;
+import org.graalvm.visualizer.graph.Figure;
+import java.awt.Color;
+import java.util.Collection;
+import java.util.List;
+
+public class EdgeColorIndexFilter extends AbstractFilter {
+
+    public static final String INPUTS = "INPUTS";
+    public static final String OUTPUTS = "OUTPUTS";
+    private final String applyTo;
+    private final Color[] colors;
+
+    public EdgeColorIndexFilter(String applyTo, Color... color) {
+        if (!applyTo.equals(INPUTS) && !applyTo.equals(OUTPUTS)) {
+            throw new IllegalArgumentException("applyTo");
+        }
+
+        this.applyTo = applyTo;
+        this.colors = color;
+    }
+
+    @Override
+    public String getName() {
+        return "Edge Color Index Filter";
+    }
+
+    @Override
+    public void apply(Diagram d) {
+        Collection<Figure> figures = d.getFigures();
+        for (Figure f : figures) {
+            Slot[] slots;
+            if (applyTo.equals(INPUTS)) {
+                List<InputSlot> inputSlots = f.getInputSlots();
+                slots = inputSlots.toArray(new Slot[inputSlots.size()]);
+            } else {
+                List<OutputSlot> outputSlots = f.getOutputSlots();
+                slots = outputSlots.toArray(new Slot[outputSlots.size()]);
+            }
+            int index = 0;
+            for (Slot slot : slots) {
+                checkCancelled();
+                if (index < colors.length && colors[index] != null) {
+                    slot.setColor(colors[index]);
+                    for (Connection c : slot.getConnections()) {
+
+                        c.setColor(colors[index]);
+                    }
+                }
+                index++;
+            }
+
+        }
+    }
+}
