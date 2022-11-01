1decd952f024dbdaff2bece8a279dc53328e7551
dutfile <113059452+dutfile@users.noreply.github.com>
2022-11-01 19:44:49 +0000

    Pretty CellML view: some work on writing a CellML to pretty CellML converter (#494) [ci skip].

--- compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/hotpath/HP_inline01.java
+++ compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/hotpath/HP_inline01.java
@@ -31,4 +31,30 @@ import org.graalvm.compiler.jtt.JTTTest;
 
 /*
  */
-public class HP_inli
+public class HP_inline01 extends JTTTest {
+
+    public static int test(int count) {
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += foo(i);
+        }
+        return sum;
+    }
+
+    public static int foo(int x) {
+        if (x < 15) {
+            return bar(x);
+        }
+        return bar(x + 1);
+    }
+
+    public static int bar(int x) {
+        return x + 1;
+    }
+
+    @Test
+    public void run0() throws Throwable {
+        runTest("test", 20);
+    }
+
+}
