3818ae63b04d528be8f0b0ce9d7d7fdef934a300
dutfile <113059452+dutfile@users.noreply.github.com>
2022-11-25 20:14:42 +0000

    Bump to dev version

---
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/NativeLibrarySupport.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.svm.core.jdk;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.graalvm.nativeimage.ImageSingletons;
+import org.graalvm.nativeimage.Platform.HOSTED_ONLY;
+import org.graalvm.nativeimage.Platforms;
+import org.graalvm.nativeimage.ProcessProperties;
+import org.graalvm.nativeimage.impl.ProcessPropertiesSupport;
+import org.graalvm.word.PointerBase;
+import org.graalvm.word.WordFactory;
+
+import com.oracle.svm.core.NeverInline;
+import com.oracle.svm.core.SubstrateOptions;
+import com.oracle.svm.core.SubstrateUtil;
+import com.oracle.svm.core.feature.AutomaticallyRegisteredImageSingleton;
+import com.oracle.svm.core.jdk.PlatformNativeLibrarySupport.NativeLibrary;
+import com.oracle.svm.core.snippets.KnownIntrinsics;
+
+@AutomaticallyRegisteredImageSingleton
+public final class NativeLibrarySupport {
+    // Essentially a revised implementation of the relevant methods in OpenJDK's ClassLoader
+
+    public interface LibraryInitializer {
+        boolean isBuiltinLibrary(String name);
+
+        void initialize(NativeLibrary lib);
+    }
+
+    public static NativeLibrarySupport singleton() {
+        return ImageSingletons.lookup(NativeLibrarySupport.class);
+    }
+
+    private final ReentrantLock lock = new ReentrantLock();
+
+    private final List<NativeLibrary> knownLibraries = new CopyOnWriteArrayList<>();
+
+    private final Deque<NativeLibrary> currentLoadContext = new ArrayDeque<>();
+
+    /** The path of the directory containing the native image. */
+    private String sysPath;
+    /** Paths derived from the {@code java.library.path} system property. */
+    private String[] usrPaths;
+
+    private LibraryInitializer libraryInitializer;
+
+    NativeLibrarySupport() {
+    }
+
+    @Platforms(HOSTED_ONLY.class)
+    public void registerLibraryInitializer(LibraryInitializer initializer) {
+        assert this.libraryInitializer == null;
+        this.libraryInitializer = initializer;
+    }
+
+    @Platforms(HOSTED_ONLY.class)
+    public void preregisterUninitializedBuiltinLibrary(String name) {
+        knownLibraries.add(PlatformNativeLibrarySupport.singleton().createLibrary(name, true));
+    }
+
+    @Platforms(HOSTED_ONLY.class)
+    public boolean isPreregisteredBuiltinLibrary(String name) {
+        return knownLibraries.stream().anyMatch(l -> l.isBuiltin() && l.getCanonicalIdentifier().equals(name));
+    }
+
+    public void loadLibraryAbsolute(File file) {
+        if (loadLibrary0(file, false)) {
+            return;
+        }
+        throw new UnsatisfiedLinkError("Can't load library: " + file);
+    }
+
+    public void loadLibraryRelative(String name) {
+        // Test if this is a built-in library
+        if (loadLibrary0(new File(name), true)) {
+            return;
+      
