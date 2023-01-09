ca4f44e4135d965e8a7999ad9dd3fd538e099c20
dutfile <113059452+dutfile@users.noreply.github.com>
2023-01-09 21:31:59 +0000

    todo done

--- tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/InspectorDebugger.java
+++ tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/InspectorDebugger.java
@@ -11,4 +11,139 @@
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICE
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.tools.chromeinspector;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.graalvm.collections.Pair;
+
+import com.oracle.truffle.api.debug.Breakpoint;
+import com.oracle.truffle.api.debug.DebugException;
+import com.oracle.truffle.api.debug.DebugScope;
+import com.oracle.truffle.api.debug.DebugStackFrame;
+import com.oracle.truffle.api.debug.DebugStackTraceElement;
+import com.oracle.truffle.api.debug.DebugValue;
+import com.oracle.truffle.api.debug.Debugger;
+import com.oracle.truffle.api.debug.DebuggerSession;
+import com.oracle.truffle.api.debug.SourceElement;
+import com.oracle.truffle.api.debug.StepConfig;
+import com.oracle.truffle.api.debug.SuspendAnchor;
+import com.oracle.truffle.api.debug.SuspendedCallback;
+import com.oracle.truffle.api.debug.SuspendedEvent;
+import com.oracle.truffle.api.debug.SuspensionFilter;
+import com.oracle.truffle.api.nodes.LanguageInfo;
+import com.oracle.truffle.api.source.Source;
+import com.oracle.truffle.api.source.SourceSection;
+import com.oracle.truffle.api.utilities.TriState;
+import com.oracle.truffle.tools.chromeinspector.InspectorExecutionContext.CancellableRunnable;
+import com.oracle.truffle.tools.chromeinspector.InspectorExecutionContext.NoSuspendedThreadException;
+import com.oracle.truffle.tools.chromeinspector.InspectorExecutionContext.SuspendedThreadExecutor;
+import com.oracle.truffle.tools.chromeinspector.ScriptsHandler.LoadScriptListener;
+import com.oracle.truffle.tools.chromeinspector.commands.Params;
+import com.oracle.truffle.tools.chromeinspector.commands.Result;
+import com.oracle.truffle.tools.chromeinspector.domains.DebuggerDomain;
+import com.oracle.truffle.tools.chromeinspector.events.Event;
+import com.oracle.truffle.tools.chromeinspector.server.CommandProcessException;
+import com.oracle.truffle.tools.chromeinspector.server.InspectServerSession.CommandPostProcessor;
+import com.oracle.truffle.tools.chromeinspector.types.CallArgument;
+import com.oracle.truffle.tools.chromeinspector.types.CallFrame;
+import com.oracle.truffle.tools.chromeinspector.types.ExceptionDetails;
+import com.oracle.truffle.tools.chromeinspector.types.Location;
+import com.oracle.truffle.tools.chromeinspector.types.RemoteObject;
+import com.oracle.truffle.tools.chromeinspector.types.Scope;
+import com.oracle.truffle.tools.chromeinspector.types.Script;
+import com.oracle.truffle.tools.chromeinspector.types.StackTrace;
+import com.oracle.truffle.tools.chromeinspector.util.LineSearch;
+import com.oracle.truffle.tools.utils.json.JSONArray;
+import com.oracle.truffle.tools.utils.json.JSONObject;
+
+public final class InspectorDebugger extends DebuggerDomain {
+
+    private static final StepConfig STEP_CONFIG = StepConfig.newBuilder().suspendAnchors(SourceElement.ROOT, SuspendAnchor.AFTER).build();
+
+    // Generic matcher of completion function
+    // (function(x){var a=[];for(var o=x;o!==null&&typeof o !== 'undefined';o=o.__proto__){
+    // a.push(Object.getOwnPropertyNames(o))
+    // };return a})(obj)
+    private static final Pattern FUNCTION_COMPLETION_PATTERN = Pattern.compile(
+                    "\\(function\\s*\\((?<x>\\w+)\\)\\s*\\{\\s*var\\s+(?<a>\\w+)\\s*=\\s*\\[\\];\\s*" +
+                                    "for\\s*\\(var\\s+(?<o>\\w+)\\s*=\\s*\\k<x>;\\s*\\k<o>\\s*\\!==\\s*null\\s*&&\\s*typeof\\s+\\k<o>\\s*\\!==\\s*.undefined.;\\k<o>\\s*=\\s*\\k<o>\\.__proto__\\)\\s*\\{" +
+                                    "\\s*\\k<a>\\.push\\(Object\\.getOwnPropertyNames\\(\\k<o>\\)\\)" +
+                                    "\\};\\s*return\\s+\\k<a>\\}\\)\\((?<object>.*)\\)$");
+
+    private final InspectorExecutionContext context;
+    private final Object suspendLock = new Object();
+    private volatile SuspendedCallbackImpl suspendedCallback;
+    private volatile DebuggerSession debuggerSession;
+    private volatile ScriptsHandler scriptsHandler;
+    private volatile BreakpointsHandler breakpointsHandler;
+    // private Scope globalScope;
+    private volatile DebuggerSuspendedInfo suspendedInfo; // Set when suspended
+    private boolean running = true;
+    private boolean runningUnwind = false;
+    private boolean silentResume = false;
+    private volatile CommandLazyResponse commandLazyResponse;
+    private final AtomicBoolean delayUnlock = new AtomicBoolean();
+    private final Phaser onSuspendPhaser = new Phaser();
+    private final BlockingQueue<CancellableRunnable> suspendThreadExecutables = new LinkedBlockingQueue<>();
+    private final ReadWriteLock domainLock;
+
+    public InspectorDebugger(InspectorExecutionContext context, boolean suspend, ReadWriteLock domainLock) {
+        this.context = context;
+        this.domainLock = domainLock;
+        context.setSuspendThreadExecutor(new SuspendedThreadExecutor() {
+            @Override
+            public void execute(CancellableRunnable executable) throws NoSuspendedThreadException {
+                try {
+                    synchronized (suspendLock) {
+                        if (running) {
+                            NoSuspendedThreadException.raise();
+                        }
+                        suspendThreadExecutables.put(executable);
+                        suspendLock.notifyAll();
+                    }
+                } catch (InterruptedException iex) {
+                    throw new RuntimeException(iex);
+                }
+            }
+        });
+        if (suspend) {
+            Lock lock = domainLock.writeLock();
+            lock.lock();
+            try {
+                startSession();
+ 
