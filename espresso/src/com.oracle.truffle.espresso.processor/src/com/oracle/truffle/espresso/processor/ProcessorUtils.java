14644dcdee2f56200b324e4d5b89665ab83e8d73
dutfile <113059452+dutfile@users.noreply.github.com>
2023-01-17 07:38:42 +0000

    bgcolor

--- espresso/src/com.oracle.truffle.espresso.processor/src/com/oracle/truffle/espresso/processor/ProcessorUtils.java
+++ espresso/src/com.oracle.truffle.espresso.processor/src/com/oracle/truffle/espresso/processor/ProcessorUtils.java
@@ -87,4 +87,89 @@ public final class ProcessorUtils {
         return "";
     }
 
-    /** Retu
+    /** Returns the given string whose first letter is lower case. */
+    public static String decapitalize(String str) {
+        if (str.length() > 0) {
+            char[] c = str.toCharArray();
+            c[0] = Character.toLowerCase(c[0]);
+            return new String(c);
+        }
+        return "";
+    }
+
+    /**
+     * Transforms a Java-like class name declaration to a member name for it. Acts the same as
+     * {@link #decapitalize(String)} for strings starting with a single capital letter, but for
+     * strings with multiple upper case characters at the beginning, it lower-cases the start until
+     * the last upper-case.
+     * <p>
+     * For example:
+     * <ul>
+     * <li>"JVMSomeThing" -> "jvmSomeThing"</li>
+     * <li>"AClass" -> "aClass"</li>
+     * <li>"ClassName" -> "className"</li>
+     * </ul>
+     */
+    public static String toMemberName(String str) {
+        if (str.length() >= 2) {
+            if (Character.isUpperCase(str.charAt(0)) && (Character.isUpperCase(str.charAt(1)))) {
+                // the first two characters are upper-case: need some special handling.
+                char[] c = str.toCharArray();
+                int i = 0;
+                while (i < c.length && Character.isUpperCase(c[i])) {
+                    // Lower case everything until we see a lower case.
+                    c[i] = Character.toLowerCase(c[i]);
+                    i++;
+                }
+                if (i != c.length) {
+                    // If we are not at the end, we went too far by one. Re-upper case the lastly
+                    // modified character.
+                    c[i - 1] = Character.toUpperCase(c[i - 1]);
+                }
+                return new String(c);
+            }
+        }
+        // Handles empty string
+        return decapitalize(str);
+    }
+
+    /**
+     * Converts a C-like name convention to a java like class name convention. In practice, this
+     * removes underscores, and converts the character following it to upper case, along with
+     * converting the first letter to upper case.
+     * <p>
+     * For example:
+     * <ul>
+     * <li>"native_member" -> "NativeMember"</li>
+     * <li>"_struct_type" -> "StructType"</li>
+     * </ul>
+     */
+    public static String removeUnderscores(String strName) {
+        StringBuilder builder = new StringBuilder();
+        int current = 0;
+        int index;
+        while ((current < strName.length()) && ((index = strName.indexOf("_", current)) >= 0)) {
+            if (index > current) {
+                String substring = strName.substring(current, index);
+                builder.append(capitalize(substring));
+            }
+            current = index + 1;
+        }
+        String substring = strName.substring(current);
+        builder.append(capitalize(substring));
+        return builder.toString();
+    }
+
+    private static String listToString(String[] strs, String separator) {
+        StringBuilder str = new StringBuilder();
+        boolean first = true;
+        for (String arg : strs) {
+            if (!first) {
+                str.append(separator);
+            }
+            str.append(arg);
+            first = false;
+        }
+        return str.toString();
+    }
+}
